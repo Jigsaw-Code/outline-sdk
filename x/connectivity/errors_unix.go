1bfbb2cb57a11771b610ad061ba1d1b11f922e84
Vinicius Fortuna <fortuna@users.noreply.github.com>
2023-05-22 12:27:02 -0400

    Restore build constraints

--- x/connectivity/errors_unix.go
+++ x/connectivity/errors_unix.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
+
 package connectivity
 
 import (
