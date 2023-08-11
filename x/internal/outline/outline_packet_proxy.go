1955bf34b72e2b0c425511716dadde17b895d36b
jyyi1 <yijunyi@google.com>
2023-08-10 23:17:12 -0400

    move x/outline to x/internal/outline

---
+++ x/internal/outline/outline_packet_proxy.go
@@ -0,0 +1,88 @@
+// Copyright 2023 Jigsaw Operations LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package outline
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/Jigsaw-Code/outline-sdk/network"
+	"github.com/Jigsaw-Code/outline-sdk/network/dnstruncate"
+	"github.com/Jigsaw-Code/outline-sdk/transport"
+	"github.com/Jigsaw-Code/outline-sdk/transport/shadowsocks"
+	"github.com/Jigsaw-Code/outline-sdk/x/connectivity"
+)
+
+type OutlinePacketProxy interface {
+	network.PacketProxy
+
+	TestConnectivityAndRefresh(resolver, domain string) error
+}
+
+type outlinePacketProxy struct {
+	network.DelegatePacketProxy
+
+	remotePktListener transport.PacketListener // this will be used in connectivity test
+	remote, fallback  network.PacketProxy
+}
+
+var _ OutlinePacketProxy = (*outlinePacketProxy)(nil)
+
+func NewOutlinePacketProxy(config *SessionConfig) (opp OutlinePacketProxy, err error) {
+	proxy := outlinePacketProxy{}
+
+	proxy.fallback, err = dnstruncate.NewPacketProxy()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create DNS truncate proxy: %w", err)
+	}
+
+	// Create Shadowsocks UDP PacketProxy
+	proxy.remotePktListener, err = NewOutlinePacketListener(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create UDP listener: %w", err)
+	}
+
+	proxy.remote, err = network.NewPacketProxyFromPacketListener(proxy.remotePktListener)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create UDP proxy: %w", err)
+	}
+
+	// Create DelegatePacketProxy
+	proxy.DelegatePacketProxy, err = network.NewDelegatePacketProxy(proxy.fallback)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create delegate UDP proxy: %w", err)
+	}
+
+	return &proxy, nil
+}
+
+func (proxy *outlinePacketProxy) TestConnectivityAndRefresh(resolver, domain string) error {
+	dialer := transport.PacketListenerDialer{Listener: proxy.remotePktListener}
+	dnsResolver := &transport.PacketDialerEndpoint{Dialer: dialer, Address: resolver}
+	_, err := connectivity.TestResolverPacketConnectivity(context.Background(), dnsResolver, domain)
+
+	if err != nil {
+		return proxy.SetProxy(proxy.fallback)
+	} else {
+		return proxy.SetProxy(proxy.remote)
+	}
+}
+
+func NewOutlinePacketListener(config *SessionConfig) (transport.PacketListener, error) {
+	ssAddress := net.JoinHostPort(config.Hostname, strconv.Itoa(config.Port))
+	return shadowsocks.NewPacketListener(&transport.UDPEndpoint{Address: ssAddress}, config.CryptoKey)
+}
--- x/outline-connectivity/main.go
+++ x/outline-connectivity/main.go
@@ -28,29 +28,13 @@ import (
 	"time"
 
 	"github.com/Jigsaw-Code/outline-sdk/transport"
-	"github.com/Jigsaw-Code/outline-sdk/transport/shadowsocks"
 	"github.com/Jigsaw-Code/outline-sdk/x/connectivity"
-	"github.com/Jigsaw-Code/outline-sdk/x/outline"
+	"github.com/Jigsaw-Code/outline-sdk/x/internal/outline"
 )
 
 var debugLog log.Logger = *log.New(io.Discard, "", 0)
 
 // var errorLog log.Logger = *log.New(os.Stderr, "[ERROR] ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
-
-func makeStreamDialer(proxyAddress string, cryptoKey *shadowsocks.EncryptionKey, prefix []byte) (transport.StreamDialer, error) {
-	proxyDialer, err := shadowsocks.NewStreamDialer(&transport.TCPEndpoint{Address: proxyAddress}, cryptoKey)
-	if err != nil {
-		return nil, err
-	}
-	if len(prefix) > 0 {
-		proxyDialer.SaltGenerator = shadowsocks.NewPrefixSaltGenerator(prefix)
-	}
-	return proxyDialer, nil
-}
-
-func makePacketListener(proxyAddress string, cryptoKey *shadowsocks.EncryptionKey) (transport.PacketListener, error) {
-	return shadowsocks.NewPacketListener(&transport.UDPEndpoint{Address: proxyAddress}, cryptoKey)
-}
 
 type jsonRecord struct {
 	// Inputs
@@ -138,7 +122,7 @@ func main() {
 	jsonEncoder.SetEscapeHTML(false)
 	// TODO: limit number of IPs. Or force an input IP?
 	for _, hostIP := range proxyIPs {
-		proxyAddress := net.JoinHostPort(hostIP.String(), fmt.Sprint(config.Port))
+		config.Hostname = hostIP.String()
 		for _, resolverHost := range strings.Split(*resolverFlag, ",") {
 			resolverHost := strings.TrimSpace(resolverHost)
 			resolverAddress := net.JoinHostPort(resolverHost, "53")
@@ -150,14 +134,14 @@ func main() {
 				var testDuration time.Duration
 				switch proto {
 				case "tcp":
-					dialer, err := makeStreamDialer(proxyAddress, config.CryptoKey, config.Prefix)
+					dialer, err := outline.NewOutlineStreamDialer(config)
 					if err != nil {
 						log.Fatalf("Failed to create StreamDialer: %v", err)
 					}
 					resolver := &transport.StreamDialerEndpoint{Dialer: dialer, Address: resolverAddress}
 					testDuration, testErr = connectivity.TestResolverStreamConnectivity(context.Background(), resolver, *domainFlag)
 				case "udp":
-					listener, err := makePacketListener(proxyAddress, config.CryptoKey)
+					listener, err := outline.NewOutlinePacketListener(config)
 					if err != nil {
 						log.Fatalf("Failed to create PacketListener: %v", err)
 					}
@@ -172,7 +156,7 @@ func main() {
 					success = true
 				}
 				record := jsonRecord{
-					Proxy:      proxyAddress,
+					Proxy:      net.JoinHostPort(hostIP.String(), fmt.Sprint(config.Port)),
 					Resolver:   resolverAddress,
 					Proto:      proto,
 					Prefix:     config.Prefix.String(),
