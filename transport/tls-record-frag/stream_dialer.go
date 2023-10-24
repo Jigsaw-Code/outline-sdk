4aada28cec6750e84e3b0e1e89bbdcb26dbfc495
Lanius-collaris <Lanius-collaris@users.noreply.github.com>
2023-10-24 10:24:06 +0800

    Several fix for tlsRecordFragWriter

--- transport/tls-record-frag/stream_dialer.go
+++ transport/tls-record-frag/stream_dialer.go
@@ -1,3 +1,17 @@
+// Copyright 2023 Jigsaw Operations LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tlsrecordfrag
 
 import (
@@ -9,13 +23,13 @@ import (
 
 type tlsRecordFragDialer struct {
 	dialer     transport.StreamDialer
-	splitPoint uint32
+	splitPoint int32
 }
 
 var _ transport.StreamDialer = (*tlsRecordFragDialer)(nil)
 
 // NewStreamDialer creates a [transport.StreamDialer] that splits the Client Hello Message
-func NewStreamDialer(dialer transport.StreamDialer, prefixBytes uint32) (transport.StreamDialer, error) {
+func NewStreamDialer(dialer transport.StreamDialer, prefixBytes int32) (transport.StreamDialer, error) {
 	if dialer == nil {
 		return nil, errors.New("argument dialer must not be nil")
 	}
--- transport/tls-record-frag/writer.go
+++ transport/tls-record-frag/writer.go
@@ -1,18 +1,32 @@
+// Copyright 2023 The Outline Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tlsrecordfrag
 
 import (
-	"errors"
+	"encoding/binary"
 	"io"
 )
 
 type tlsRecordFragWriter struct {
 	writer      io.Writer
-	prefixBytes uint32
+	prefixBytes int32
 }
 
-const maxRecordLength = 16384
+const maxRecordLength = 16384 //For the fragments, not for the reassembled record
 
-func NewWriter(writer io.Writer, prefixBytes uint32) *tlsRecordFragWriter {
+func NewWriter(writer io.Writer, prefixBytes int32) *tlsRecordFragWriter {
 	return &tlsRecordFragWriter{writer, prefixBytes}
 }
 
@@ -30,39 +44,37 @@ func (w *tlsRecordFragWriter) dontFrag(first []byte, source io.Reader) (written 
 
 func (w *tlsRecordFragWriter) ReadFrom(source io.Reader) (written int64, err error) {
 	if 0 < w.prefixBytes {
-		var first [5]byte
-		_, err := io.ReadFull(source, first[:])
+		var recordHeader [5]byte
+		_, err := io.ReadFull(source, recordHeader[:])
 		if err != nil {
 			return 0, err
 		}
-		recordLength := uint32(first[3]) << 8 | uint32(first[4])
+		recordLength := int32(binary.BigEndian.Uint16(recordHeader[3:]))
 		if w.prefixBytes >= recordLength {
-			return w.dontFrag(first[:], source)
+			return w.dontFrag(recordHeader[:], source)
 		}
 		if recordLength > maxRecordLength {
-			return 0, errors.New("Broken handshake message")
+			return w.dontFrag(recordHeader[:], source)
 		}
+		// Allocate buffer that fits the entire record after the split (2*header + payload).
 		buf := make([]byte, recordLength+10)
+		header := recordHeader[:3]
+
+		copy(buf, header)
+		binary.BigEndian.PutUint16(buf[3:], uint16(w.prefixBytes))
 		n2, err := io.ReadFull(source, buf[5:5+w.prefixBytes])
 		if err != nil {
 			w.prefixBytes = 0
 			return 0, err
 		}
-		n3, err := io.ReadFull(source, buf[10+w.prefixBytes:])
+
+		copy(buf[5+n2:], header)
+		binary.BigEndian.PutUint16(buf[5+n2+3:], uint16(recordLength-w.prefixBytes))
+		_, err = io.ReadFull(source, buf[10+w.prefixBytes:])
 		if err != nil {
 			w.prefixBytes = 0
 			return 0, err
 		}
-
-		header := first[:3]
-
-		copy(buf, header)
-		buf[3] = byte(uint32(n2) >> 8)
-		buf[4] = byte(uint32(n2) & 0xff)
-
-		copy(buf[5+n2:], header)
-		buf[5+n2+3] = byte(uint32(n3) >> 8)
-		buf[5+n2+4] = byte(uint32(n3) & 0xff)
 
 		tmp, err := w.writer.Write(buf)
 		w.prefixBytes = 0
@@ -79,22 +91,20 @@ func (w *tlsRecordFragWriter) ReadFrom(source io.Reader) (written int64, err err
 func (w *tlsRecordFragWriter) Write(data []byte) (written int, err error) {
 	length := len(data)
 	if length > 5+maxRecordLength {
-		return 0, errors.New("Broken handshake message")
+		return w.writer.Write(data)
 	}
-	if 0 < w.prefixBytes && w.prefixBytes < uint32(length -5) {
+	if 0 < w.prefixBytes && int(w.prefixBytes) < length-5 {
 		buf := make([]byte, length+5)
 		header := data[:3]
 		record1 := data[5 : 5+w.prefixBytes]
 		record2 := data[5+w.prefixBytes:]
 
 		copy(buf, header)
-		buf[3] = byte(w.prefixBytes >> 8)
-		buf[4] = byte(w.prefixBytes & 0xff)
+		binary.BigEndian.PutUint16(buf[3:], uint16(w.prefixBytes))
 		copy(buf[5:], record1)
 
 		copy(buf[5+w.prefixBytes:], header)
-		buf[5+3+w.prefixBytes] = byte(len(record2) >> 8)
-		buf[5+4+w.prefixBytes] = byte(len(record2) & 0xff)
+		binary.BigEndian.PutUint16(buf[5+3+w.prefixBytes:], uint16(len(record2)))
 		copy(buf[5+5+w.prefixBytes:], record2)
 
 		w.prefixBytes = 0
