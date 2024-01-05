4e22395f70b2514b84b2449615c23be888ccf9b9
amir gh <amir@we-pn.com>
2024-01-04 21:14:57 -0800

    generalize config sanitizer remove gethostnames

--- x/config/config_test.go
+++ x/config/config_test.go
@@ -16,13 +16,19 @@ func TestSanitizeConfig(t *testing.T) {
 	sanitizedConfig, err := SanitizeConfig("split:5|ss://jhvdsjkfhvkhsadvf@example.com:1234?prefix=HTTP1.1")
 	fmt.Println(sanitizedConfig)
 	require.NoError(t, err)
-	require.Equal(t, "split:5|ss://[redacted]@example.com:1234?prefix=HTTP1.1", sanitizedConfig)
+	require.Equal(t, "split:5|ss://redacted@example.com:1234?prefix=HTTP1.1", sanitizedConfig)
 
 	// Test that a valid config is accepted.
 	sanitizedConfig, err = SanitizeConfig("split:5|ss://jhvdsjkfhvkhsadvf@example.com:1234")
 	fmt.Println(sanitizedConfig)
 	require.NoError(t, err)
-	require.Equal(t, "split:5|ss://[redacted]@example.com:1234", sanitizedConfig)
+	require.Equal(t, "split:5|ss://redacted@example.com:1234", sanitizedConfig)
+
+	// Test that a valid config is accepted.
+	sanitizedConfig, err = SanitizeConfig("split:5|vless://ac08785d-203d-4db4-915c-eb4e23435fd62@example.com:443?path=%2Fvless&security=tls&encryption=none&alpn=h2&host=sub.hello.com&fp=chrome&type=ws&sni=sub.hello.com#vless-ws-tls-cdn")
+	fmt.Println(sanitizedConfig)
+	require.NoError(t, err)
+	require.Equal(t, "split:5|vless://redacted@example.com:443?path=%2Fvless&security=tls&encryption=none&alpn=h2&host=sub.hello.com&fp=chrome&type=ws&sni=sub.hello.com#vless-ws-tls-cdn", sanitizedConfig)
 
 	// Test that a valid config is accepted.
 	sanitizedConfig, err = SanitizeConfig("split:5|tlsfrag:5")
@@ -31,26 +37,6 @@ func TestSanitizeConfig(t *testing.T) {
 	require.Equal(t, "split:5|tlsfrag:5", sanitizedConfig)
 
 	// Test that an invalid config is rejected.
-	_, err = SanitizeConfig("ss://[redacted]@")
-	require.Error(t, err)
-}
-
-func TestGetHostnamesFromConfig(t *testing.T) {
-	// Test that empty config is accepted.
-	_, err := GetHostnamesFromConfig("")
-	require.NoError(t, err)
-
-	// Test that a valid config is accepted.
-	hostname, err := GetHostnamesFromConfig("ss://example.com:1234?prefix=HTTP1.1|socks5://example2.com:1234")
-	fmt.Println(hostname)
-	require.NoError(t, err)
-	require.Equal(t, []string{"example.com", "example2.com"}, hostname)
-
-	//Test that an invalid config is rejected.
-	_, err = GetHostnamesFromConfig("|fadsf|")
-	require.Error(t, err)
-
-	//Test that an invalid config is rejected.
-	_, err = GetHostnamesFromConfig("|")
+	_, err = SanitizeConfig("::hghg")
 	require.Error(t, err)
 }
