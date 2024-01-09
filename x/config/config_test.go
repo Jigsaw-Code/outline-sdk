9d01b305802e20c533cf2bd8dcfee49c553800ac
amir gh <amir@we-pn.com>
2024-01-09 12:30:40 -0800

    add test + code cleanup

--- x/config/config_test.go
+++ x/config/config_test.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"fmt"
+	"encoding/base64"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -14,29 +14,40 @@ func TestSanitizeConfig(t *testing.T) {
 
 	// Test that a valid config is accepted.
 	sanitizedConfig, err := SanitizeConfig("split:5|ss://jhvdsjkfhvkhsadvf@example.com:1234?prefix=HTTP1.1")
-	fmt.Println(sanitizedConfig)
 	require.NoError(t, err)
-	require.Equal(t, "split:5|ss://redacted@example.com:1234?prefix=HTTP1.1", sanitizedConfig)
+	require.Equal(t, "split:5|ss://REDACTED@example.com:1234?prefix=HTTP1.1", sanitizedConfig)
 
 	// Test that a valid config is accepted.
 	sanitizedConfig, err = SanitizeConfig("split:5|ss://jhvdsjkfhvkhsadvf@example.com:1234")
-	fmt.Println(sanitizedConfig)
 	require.NoError(t, err)
-	require.Equal(t, "split:5|ss://redacted@example.com:1234", sanitizedConfig)
+	require.Equal(t, "split:5|ss://REDACTED@example.com:1234", sanitizedConfig)
 
 	// Test that a valid config is accepted.
 	sanitizedConfig, err = SanitizeConfig("split:5|vless://ac08785d-203d-4db4-915c-eb4e23435fd62@example.com:443?path=%2Fvless&security=tls&encryption=none&alpn=h2&host=sub.hello.com&fp=chrome&type=ws&sni=sub.hello.com#vless-ws-tls-cdn")
-	fmt.Println(sanitizedConfig)
 	require.NoError(t, err)
-	require.Equal(t, "split:5|vless://redacted@example.com:443?path=%2Fvless&security=tls&encryption=none&alpn=h2&host=sub.hello.com&fp=chrome&type=ws&sni=sub.hello.com#vless-ws-tls-cdn", sanitizedConfig)
+	require.Equal(t, "split:5|vless://REDACTED@example.com:443?path=%2Fvless&security=tls&encryption=none&alpn=h2&host=sub.hello.com&fp=chrome&type=ws&sni=sub.hello.com#vless-ws-tls-cdn", sanitizedConfig)
 
 	// Test that a valid config is accepted.
 	sanitizedConfig, err = SanitizeConfig("split:5|tlsfrag:5")
-	fmt.Println(sanitizedConfig)
 	require.NoError(t, err)
 	require.Equal(t, "split:5|tlsfrag:5", sanitizedConfig)
+
+	// Test that a valid config is accepted.
+	sanitizedConfig, err = SanitizeConfig("transport://hjdbfjhbqfjheqrf")
+	require.NoError(t, err)
+	require.Equal(t, "transport://UNKNOWN", sanitizedConfig)
 
 	// Test that an invalid config is rejected.
 	_, err = SanitizeConfig("::hghg")
 	require.Error(t, err)
+}
+
+func TestSanitizeURL(t *testing.T) {
+	encoded := base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString([]byte("jhvdsjkfhvkhsadvf@example.com:1234?prefix=HTTP1.1"))
+	//decoded, _ := base64.URLEncoding.WithPadding(base64.NoPadding).DecodeString(encoded)
+	u, err := parseConfigPart("ss://" + string(encoded))
+	require.NoError(t, err)
+	sanitizedURL, err := sanitizeURL(u)
+	require.NoError(t, err)
+	require.Equal(t, "ss://REDACTED@example.com:1234?prefix=HTTP1.1", sanitizedURL)
 }
