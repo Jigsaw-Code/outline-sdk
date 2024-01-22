f3a273f30e4cd20b7b08217063ada1f62612a902
amir gh <amir@we-pn.com>
2024-01-21 21:15:46 -0800

    handle connectivity.ConnectivityError nil value

--- x/examples/outline-connectivity-app/shared_backend/main.go
+++ x/examples/outline-connectivity-app/shared_backend/main.go
@@ -17,7 +17,6 @@ package shared_backend
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -74,6 +73,9 @@ type ConnectivityTestRequest struct {
 func ConnectivityTest(request ConnectivityTestRequest) ([]ConnectivityTestResult, error) {
 	var result ConnectivityTestResult
 	var results []ConnectivityTestResult
+	var conTestResult *ConnectivityTestError
+	var conError *connectivity.ConnectivityError
+	var testDuration time.Duration
 
 	sanitizedConfig, err := config.SanitizeConfig(request.Transport)
 	if err != nil {
@@ -83,27 +85,28 @@ func ConnectivityTest(request ConnectivityTestRequest) ([]ConnectivityTestResult
 	for _, resolverHost := range request.Resolvers {
 		resolverHost := strings.TrimSpace(resolverHost)
 		resolverAddress := net.JoinHostPort(resolverHost, "53")
-		fmt.Printf("ResolverAddress: %v\n", resolverAddress)
 
 		if request.Protocols.TCP {
 			testTime := time.Now()
-			var testErr error
-			var testDuration time.Duration
-
 			streamDialer, err := config.NewStreamDialer(request.Transport)
 			if err != nil {
-				//log.Fatalf("Failed to create StreamDialer: %v", err)
 				testDuration = time.Duration(0)
-				testErr = err
+				conTestResult = &ConnectivityTestError{Msg: err.Error()}
 			} else {
 				resolver := dns.NewTCPResolver(streamDialer, resolverAddress)
-				startTime := time.Now()
-				// TODO: report question fail
-				testErr, _ := connectivity.TestConnectivityWithResolver(context.Background(), resolver, request.Domain)
-				testDuration := time.Since(startTime)
-				//testDuration, testErr = connectivity.TestResolverStreamConnectivity(context.Background(), resolver, resolverAddress)
-				fmt.Printf("TestDuration: %v\n", testDuration)
-				fmt.Printf("TestError: %v\n", testErr)
+				conError, err = connectivity.TestConnectivityWithResolver(context.Background(), resolver, request.Domain)
+				if err != nil {
+					testDuration = time.Duration(0)
+					conTestResult = &ConnectivityTestError{Msg: err.Error()}
+					log.Printf("TCP question failed: %v\n", err)
+				} else {
+					testDuration = time.Since(testTime)
+					if conError == nil {
+						conTestResult = nil
+					} else {
+						conTestResult = &ConnectivityTestError{conError.Op, conError.PosixError, unwrapAll(conError).Error()}
+					}
+				}
 			}
 			result = ConnectivityTestResult{
 				Transport:  sanitizedConfig,
@@ -111,52 +114,55 @@ func ConnectivityTest(request ConnectivityTestRequest) ([]ConnectivityTestResult
 				Proto:      "tcp",
 				Time:       testTime.UTC().Truncate(time.Second),
 				DurationMs: testDuration.Milliseconds(),
-				Error:      makeErrorRecord(testErr),
+				Error:      conTestResult,
 			}
 			results = append(results, result)
 		}
 
 		if request.Protocols.UDP {
 			testTime := time.Now()
-			var testErr error
-			var testDuration time.Duration
-
 			packetDialer, err := config.NewPacketDialer(request.Transport)
 			if err != nil {
-				//log.Fatalf("Failed to create PacketDialer: %v", err)
 				testDuration = time.Duration(0)
-				testErr = err
+				conTestResult = &ConnectivityTestError{Msg: err.Error()}
 			} else {
 				resolver := dns.NewUDPResolver(packetDialer, resolverAddress)
-				startTime := time.Now()
-				// TODO: report question fail
-				testErr, _ := connectivity.TestConnectivityWithResolver(context.Background(), resolver, request.Domain)
-				testDuration := time.Since(startTime)
-				//testDuration, testErr = connectivity.TestResolverStreamConnectivity(context.Background(), resolver, resolverAddress)
-				fmt.Printf("TestDuration: %v\n", testDuration)
-				fmt.Printf("TestError: %v\n", testErr)
+				conError, err = connectivity.TestConnectivityWithResolver(context.Background(), resolver, request.Domain)
+				if err != nil {
+					log.Printf("UDP question failed: %v\n", err)
+					testDuration = time.Duration(0)
+					conTestResult = &ConnectivityTestError{Msg: err.Error()}
+				} else {
+					testDuration = time.Since(testTime)
+					if conError == nil {
+						conTestResult = nil
+					} else {
+						conTestResult = &ConnectivityTestError{conError.Op, conError.PosixError, unwrapAll(conError).Error()}
+					}
+				}
 			}
-
 			result = ConnectivityTestResult{
 				Transport:  sanitizedConfig,
 				Resolver:   resolverAddress,
 				Proto:      "udp",
 				Time:       testTime.UTC().Truncate(time.Second),
 				DurationMs: testDuration.Milliseconds(),
-				Error:      makeErrorRecord(testErr),
+				Error:      conTestResult,
 			}
 			results = append(results, result)
 		}
 	}
+	reportResults(results, request.ReportTo)
+	return results, nil
+}
+
+func reportResults(results []ConnectivityTestResult, reportTo string) {
 	for _, result := range results {
-		fmt.Printf("Result: %v\n", result)
 		var r report.Report = result
-		u, err := url.Parse(request.ReportTo)
+		u, err := url.Parse(reportTo)
 		if err != nil {
 			log.Printf("Expected no error, but got: %v", err)
-			//return results, errors.New("failed to parse collector URL")
 		}
-		fmt.Println("Parsed URL: ", u.String())
 		if u.String() != "" {
 			remoteCollector := &report.RemoteCollector{
 				CollectorURL: u,
@@ -175,12 +181,9 @@ func ConnectivityTest(request ConnectivityTestRequest) ([]ConnectivityTestResult
 			err = c.Collect(context.Background(), r)
 			if err != nil {
 				log.Printf("Failed to collect report: %v\n", err)
-				//return results, errors.New("failed to collect report")
 			}
 		}
 	}
-
-	return results, nil
 }
 
 type PlatformMetadata struct {
@@ -191,23 +194,10 @@ func Platform() PlatformMetadata {
 	return PlatformMetadata{OS: runtime.GOOS}
 }
 
-func makeErrorRecord(err error) *ConnectivityTestError {
+func unwrapAll(err error) error {
 	if err == nil {
 		return nil
 	}
-	var record = new(ConnectivityTestError)
-	var testErr *connectivity.ConnectivityError
-	if errors.As(err, &testErr) {
-		record.Op = testErr.Op
-		record.PosixError = testErr.PosixError
-		record.Msg = unwrapAll(testErr).Error()
-	} else {
-		record.Msg = err.Error()
-	}
-	return record
-}
-
-func unwrapAll(err error) error {
 	for {
 		unwrapped := errors.Unwrap(err)
 		if unwrapped == nil {
