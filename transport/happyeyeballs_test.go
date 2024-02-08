4496491b2730df1c4b45db0d4367e74d0fa2bcd3
Vinicius Fortuna <fortuna@users.noreply.github.com>
2024-02-08 14:05:06 -0500

    Add tests

---
+++ transport/happyeyeballs_test.go
@@ -0,0 +1,93 @@
+// Copyright 2024 Jigsaw Operations LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package transport
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHappyEyeballsStreamDialer_DialStream(t *testing.T) {
+	t.Run("Works with IPv4 hosts", func(t *testing.T) {
+		var dialedAddr string
+		dialer := HappyEyeballsStreamDialer{
+			Dialer: FuncStreamDialer(func(ctx context.Context, addr string) (StreamConn, error) {
+				dialedAddr = addr
+				return nil, nil
+			}),
+		}
+		_, err := dialer.DialStream(context.Background(), "8.8.8.8:53")
+		require.NoError(t, err)
+		require.Equal(t, "8.8.8.8:53", dialedAddr)
+	})
+
+	t.Run("Works with IPv6 hosts", func(t *testing.T) {
+		var dialedAddr string
+		dialer := HappyEyeballsStreamDialer{
+			Dialer: FuncStreamDialer(func(ctx context.Context, addr string) (StreamConn, error) {
+				dialedAddr = addr
+				return nil, nil
+			}),
+		}
+		_, err := dialer.DialStream(context.Background(), "[2001:4860:4860::8888]:53")
+		require.NoError(t, err)
+		require.Equal(t, "[2001:4860:4860::8888]:53", dialedAddr)
+	})
+
+	t.Run("Prefer IPv6", func(t *testing.T) {
+		var dialedAddr string
+		dialer := HappyEyeballsStreamDialer{
+			Dialer: FuncStreamDialer(func(ctx context.Context, addr string) (StreamConn, error) {
+				dialedAddr = addr
+				return nil, nil
+			}),
+			LookupIPv6: func(ctx context.Context, host string) ([]net.IP, error) {
+				return []net.IP{net.ParseIP("2001:4860:4860::8888")}, nil
+			},
+			LookupIPv4: func(ctx context.Context, host string) ([]net.IP, error) {
+				return []net.IP{net.ParseIP("8.8.8.8")}, nil
+			},
+		}
+		_, err := dialer.DialStream(context.Background(), "dns.google:53")
+		require.NoError(t, err)
+		require.Equal(t, "[2001:4860:4860::8888]:53", dialedAddr)
+	})
+
+	t.Run("Use IPv4 if IPv6 hangs", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		var dialedAddr string
+		dialer := HappyEyeballsStreamDialer{
+			Dialer: FuncStreamDialer(func(ctx context.Context, addr string) (StreamConn, error) {
+				dialedAddr = addr
+				return nil, nil
+			}),
+			LookupIPv6: func(ctx context.Context, host string) ([]net.IP, error) {
+				// Make it hang.
+				<-ctx.Done()
+				return []net.IP{net.ParseIP("2001:4860:4860::8888")}, nil
+			},
+			LookupIPv4: func(ctx context.Context, host string) ([]net.IP, error) {
+				return []net.IP{net.ParseIP("8.8.8.8")}, nil
+			},
+		}
+		_, err := dialer.DialStream(ctx, "dns.google:53")
+		require.NoError(t, err)
+		require.Equal(t, "8.8.8.8:53", dialedAddr)
+	})
+}
