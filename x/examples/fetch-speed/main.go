e60f5f598b17651273a451c9525a74476e5723d1
nima <nima@asl19.org>
2024-02-21 21:28:34 -0500

    Remove an unnecessary flag

--- x/examples/fetch-speed/main.go
+++ x/examples/fetch-speed/main.go
@@ -42,23 +42,12 @@ func init() {
 func main() {
 	verboseFlag := flag.Bool("v", false, "Enable debug output")
 	transportFlag := flag.String("transport", "", "Transport config")
-	addressFlag := flag.String("address", "", "Address to connect to. If empty, use the URL authority")
 	methodFlag := flag.String("method", "GET", "The HTTP method to use")
 
 	flag.Parse()
 
 	if *verboseFlag {
 		debugLog = *log.New(os.Stderr, "[DEBUG] ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
-	}
-	var overrideHost, overridePort string
-	if *addressFlag != "" {
-		var err error
-		overrideHost, overridePort, err = net.SplitHostPort(*addressFlag)
-		if err != nil {
-			// Fail to parse. Assume the flag is host only.
-			overrideHost = *addressFlag
-			overridePort = ""
-		}
 	}
 
 	url := flag.Arg(0)
@@ -76,12 +65,6 @@ func main() {
 		host, port, err := net.SplitHostPort(addr)
 		if err != nil {
 			return nil, fmt.Errorf("invalid address: %w", err)
-		}
-		if overrideHost != "" {
-			host = overrideHost
-		}
-		if overridePort != "" {
-			port = overridePort
 		}
 		if !strings.HasPrefix(network, "tcp") {
 			return nil, fmt.Errorf("protocol not supported: %v", network)
