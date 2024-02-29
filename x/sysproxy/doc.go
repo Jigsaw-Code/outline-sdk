cc694c9ee0c7edab50cbeef597bbffd08d61d573
amir gh <amir@we-pn.com>
2024-02-29 13:27:06 -0800

    moved package docs to doc.go

--- x/sysproxy/doc.go
+++ x/sysproxy/doc.go
@@ -1,39 +1,36 @@
+// Copyright 2024 Jigsaw Operations LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /*
 Package sysproxy provides a simple interface to set/unset system-wide proxy settings.
 
 # Platform Support
+
 Currently this package supports desktop platforms only. The following platforms are supported:
-- macOS
-- Linux (Gnome is the only supported desktop environment for now.)
-- Windows
+  - macOS
+  - Linux (Gnome)
+  - Windows
 
 # Usage
 
-- To set up system-wide proxy settings, use the `SetProxy` function. This function takes two arguments: the IP address and the port of the proxy server.
-`SetProxy(ip string, port string) error`
+To set up system-wide proxy settings, use the [SetProxy] function. This function takes two arguments: the IP address and the port of the proxy server.
 
-- To unset system-wide proxy settings, use the `UnsetProxy` function.
-`UnsetProxy() error`
+To unset system-wide proxy settings, use the [UnsetProxy] function.
+To ensure that the system-wide proxy settings are unset upon program termination, it is recommended to call:
 
-You need to also clean up the proxy settings when you are done with the proxy server. This is important because the proxy settings will remain in place even after the proxy server is stopped.
-The following example demonstrates how to safely unset proxy upon program termination:
+	defer UnsetProxy()
 
-```go
-func main () {
-	// Create channels to receive signals
-	done := make(chan bool, 1)
-	sigs := make(chan os.Signal, 1)
-	sysproxy.SafeCloseProxy(done, sigs)
-
-	// Your code here
-
-	// Send a signal to the program to terminate
-	sigs<-syscall.SIGTERM
-	// Wait for the program to terminate
-	<-done
-}
-
-```
+when the application starts.
 */
-
 package sysproxy
