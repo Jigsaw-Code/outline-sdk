6a5680c5394ab2aee457ba17c00818ed358bbcfc
amir gh <amir@we-pn.com>
2024-02-29 13:11:31 -0800

    add proxyType and clean up code for Darwin

--- x/sysproxy/sysproxy_darwin.go
+++ x/sysproxy/sysproxy_darwin.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Jigsaw Operations LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //go:build darwin
 
 package sysproxy
@@ -9,19 +23,25 @@ import (
 	"strings"
 )
 
+type ProxyType string
+
+const (
+	httpProxyType  ProxyType = "web"
+	httpsProxyType ProxyType = "secureweb"
+)
+
 func SetProxy(ip string, port string) error {
-	// Execute macOS specific commands to set proxy
 	// Get the active network interface
-	activeInterface, err := getActiveNetworkInterfaceMacOS()
+	activeInterface, err := getActiveNetworkInterface()
 	if err != nil {
 		return err
 	}
 
 	// Set the web proxy and secure web proxy
-	if err := setProxyMacOS("web", activeInterface, ip, port); err != nil {
+	if err := setProxyCommand(httpProxyType, activeInterface, ip, port); err != nil {
 		return err
 	}
-	if err := setProxyMacOS("secureweb", activeInterface, ip, port); err != nil {
+	if err := setProxyCommand(httpsProxyType, activeInterface, ip, port); err != nil {
 		return err
 	}
 
@@ -29,18 +49,17 @@ func SetProxy(ip string, port string) error {
 }
 
 func UnsetProxy() error {
-	// Execute macOS specific commands to unset proxy
 	// Get the active network interface
-	activeInterface, err := getActiveNetworkInterfaceMacOS()
+	activeInterface, err := getActiveNetworkInterface()
 	if err != nil {
 		return err
 	}
 
-	// Set the web proxy and secure web proxy
-	if err := removeProxyMacOS("web", activeInterface); err != nil {
+	// Unset the web proxy and secure web proxy
+	if err := removeProxyCommand(httpProxyType, activeInterface); err != nil {
 		return err
 	}
-	if err := removeProxyMacOS("secureweb", activeInterface); err != nil {
+	if err := removeProxyCommand(httpsProxyType, activeInterface); err != nil {
 		return err
 	}
 
@@ -48,8 +67,9 @@ func UnsetProxy() error {
 }
 
 // getActiveNetworkInterface finds the active network interface using shell commands.
-func getActiveNetworkInterfaceMacOS() (string, error) {
-	cmd := "sh -c \"networksetup -listnetworkserviceorder | grep `route -n get 0.0.0.0 | grep 'interface' | cut -d ':' -f2` -B 1 | head -n 1 | cut -d ' ' -f2\""
+// https://keith.github.io/xcode-man-pages/networksetup.8.html#listnetworkserviceorder
+func getActiveNetworkInterface() (string, error) {
+	cmd := "networksetup -listnetworkserviceorder | grep `route -n get 0.0.0.0 | grep 'interface' | cut -d ':' -f2` -B 1 | head -n 1 | cut -d ' ' -f2"
 	out, err := exec.Command("bash", "-c", cmd).Output()
 	if err != nil {
 		return "", err
@@ -57,17 +77,10 @@ func getActiveNetworkInterfaceMacOS() (string, error) {
 	return strings.TrimSpace(string(out)), nil
 }
 
-// setProxyMacOS sets the specified type of proxy on the given network interface.
-func setProxyMacOS(proxyType string, interfaceName string, ip string, port string) error {
-	var cmdStr string
-	if proxyType == "web" {
-		cmdStr = fmt.Sprintf("networksetup -setwebproxy \"%s\" %s %s", interfaceName, ip, port)
-	} else if proxyType == "secureweb" {
-		cmdStr = fmt.Sprintf("networksetup -setsecurewebproxy \"%s\" %s %s", interfaceName, ip, port)
-	} else {
-		return fmt.Errorf("unknown proxy type: %s", proxyType)
-	}
-
+// setProxyCommand sets the specified type of proxy on the given network interface.
+// https://keith.github.io/xcode-man-pages/networksetup.8.html#getsecurewebproxy
+func setProxyCommand(ptype ProxyType, interfaceName string, ip string, port string) error {
+	cmdStr := fmt.Sprintf("networksetup -set%sproxy \"%s\" %s %s", ptype, interfaceName, ip, port)
 	cmd := exec.Command("bash", "-c", cmdStr)
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
@@ -79,16 +92,10 @@ func setProxyMacOS(proxyType string, interfaceName string, ip string, port strin
 	return nil
 }
 
-func removeProxyMacOS(proxyType string, interfaceName string) error {
-	var cmdStr string
-	if proxyType == "web" {
-		cmdStr = fmt.Sprintf("networksetup -setwebproxystate \"%s\" off", interfaceName)
-	} else if proxyType == "secureweb" {
-		cmdStr = fmt.Sprintf("networksetup -setsecurewebproxystate \"%s\" off", interfaceName)
-	} else {
-		return fmt.Errorf("unknown proxy type: %s", proxyType)
-	}
-
+// removeProxyCommand unsets the specified type of proxy from the given network interface.
+// https://keith.github.io/xcode-man-pages/networksetup.8.html#setwebproxystate
+func removeProxyCommand(ptype ProxyType, interfaceName string) error {
+	cmdStr := fmt.Sprintf("networksetup -set%sproxystate \"%s\" off", ptype, interfaceName)
 	cmd := exec.Command("bash", "-c", cmdStr)
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
