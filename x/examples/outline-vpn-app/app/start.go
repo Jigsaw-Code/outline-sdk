57dff340d6f118c8af06c6572059dd61120b5cbc
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-03-13 10:15:14 -0400

    changes

--- x/examples/outline-vpn-app/app/start.go
+++ x/examples/outline-vpn-app/app/start.go
@@ -1,36 +1,45 @@
 package app
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"io"
+	"log"
 	"net/http"
-	"os"
 
 	"github.com/Jigsaw-Code/outline-sdk/x/config"
 	"github.com/Jigsaw-Code/outline-sdk/x/mobileproxy"
 	"github.com/a-h/templ"
+	"github.com/labstack/gommon/bytes"
 )
 
-var appAddress = ":8080"
-var proxyAddress = ":8181"
-var systemAddress = ":8282"
+var appAddress = "[::1]:8080"
+var proxyAddress = "[::1]:8181"
+var systemAddress = "[::1]:8282"
 
 var proxyDisconnectTimeoutSecond = 5
-var systemTunnelEndpoint = "${systemAddress}/tunnel/${proxyAddress}"
+var systemTunnelEndpoint = "/vpn/"
+
+type VPNController struct {
+	ID     string
+	Proxy  *mobileproxy.Proxy
+	Config VPNConfig
+}
+
+type VPNConfig struct {
+	Source string
+	Target string
+}
 
 func Start() {
-	vpn := VPNController{}
+	vpn := VPNController{ID: ""}
 
 	http.Handle("/", templ.Handler(serverView()))
 	http.Handle("/connection/ss://<my-shadowsocks-key>", http.HandlerFunc(vpn.handleConnectionEndpoint))
 	http.Handle("/disconnection/ss://<my-shadowsocks-key>", http.HandlerFunc(vpn.handleDisconnectionEndpoint))
 
 	http.ListenAndServe(appAddress, nil)
-}
-
-type VPNController struct {
-	proxy  *mobileproxy.Proxy
-	tunnel io.Reader
 }
 
 func (vpn *VPNController) handleConnectionEndpoint(responseWriter http.ResponseWriter, request *http.Request) {
@@ -48,34 +57,45 @@ func (vpn *VPNController) handleConnectionEndpoint(responseWriter http.ResponseW
 		return
 	}
 
-	vpn.proxy = proxy
-
-	// TODO: why this code isn't very good at all!
-	tunnel, err := os.Open("tunnel.sock")
-
-	if err != nil {
-		// do something
-	}
-
-	vpn.tunnel = tunnel
+	vpn.Proxy = proxy
+	vpn.Config = VPNConfig{Source: "*", Target: proxyAddress}
 
 	// TODO: implement system vpn tunnel service
-	// => POST /tunnel/URL forward all non-local traffic to that URL
-	// => DELETE /tunnel/URL gracefully shuts down the tunnel
+	// => POST /vpn { config: { source: "	", target: "	" } }
+	// create a new vpn configuration, return ID
+	// => PATCH /vpn/:id { start: true }
+	// update vpn configuration, including turn it on or off
 
 	// => apple box uses VPN API
 	// => kotlin box uses SSH tunnel (for now)
 
 	// ! these boxes will be reusable across VPN apps !
-	http.NewRequest("POST", systemTunnelEndpoint, vpn.tunnel)
+
+	if vpn.ID == "" {
+		payload, err := json.Marshal(vpn.Config)
+		response, err := http.Post(systemTunnelEndpoint, "application/json", bytes.NewReader(payload))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		vpn.ID = string(body)
+	} else {
+		toggleConnection(*vpn, true)
+	}
 
 	writeTemplate(disconnectionButton, responseWriter)
 }
 
 func (vpn *VPNController) handleDisconnectionEndpoint(responseWriter http.ResponseWriter, _ *http.Request) {
-	http.NewRequest("DELETE", systemTunnelEndpoint, vpn.tunnel)
+	toggleConnection(*vpn, false)
 
-	vpn.proxy.Stop(proxyDisconnectTimeoutSecond)
+	vpn.Proxy.Stop(proxyDisconnectTimeoutSecond)
 
 	writeTemplate(connectionButton, responseWriter)
 }
@@ -84,4 +104,22 @@ func (vpn *VPNController) handleDisconnectionEndpoint(responseWriter http.Respon
 func writeTemplate(template func() templ.Component, writer io.Writer) {
 	component := template()
 	component.Render(context.Background(), writer)
+}
+
+func toggleConnection(vpn VPNController, state bool) {
+	payload, err := json.Marshal(map[string]interface{}{
+		"start": state,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request, err := http.NewRequest(http.MethodPut, systemTunnelEndpoint+vpn.ID, bytes.NewReader(payload))
+	request.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{}
+	client.Do(request)
 }
