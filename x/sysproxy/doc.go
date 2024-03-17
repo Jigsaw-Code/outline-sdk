34ce3c32e043b3c8545bba91c32d1e762cda3f01
amir gh <amir@we-pn.com>
2024-03-16 19:44:48 -0700

    updated doc.go

--- x/sysproxy/doc.go
+++ x/sysproxy/doc.go
@@ -22,6 +22,38 @@ Currently this package supports desktop platforms only. The following platforms 
   - Linux (Gnome)
   - Windows
 
+# macOS
+
+To configure proxy settings on macOS, we use networksetup utility with following options. To set web proxy:
+
+	networksetup -setwebproxy <networkservice> <domain> <portnumber>
+
+To set secure web proxy:
+
+	networksetup -setsecurewebproxy <networkservice> <domain> <portnumber>
+
+For more information, see the link [here].
+
+# Linux
+
+Currently only GNOME is supported. This package uses gsettings untility to setup proxy settings. The following commands are used to set proxy settings:
+
+	gsetting set org.gnome.system.proxy.http host 'proxy.example.com'
+	gsetting set org.gnome.system.proxy.http port 8080
+
+The following parameters can be set for other types of proxies:
+
+	org.gnome.system.proxy.https
+	org.gnome.system.proxy.ftp
+	org.gnome.system.proxy.socks
+
+For more information, you can checkout the documentation for [gsettings] and its [configuration].
+
+# Windows
+
+On Windows, the package uses HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings + InternetSetOptionW
+to setup proxy settings. For more information, you can checkout the documentation for [InternetSetOptionW].
+
 # Usage
 
 To set up system-wide proxy settings, use the [SetProxy] function. This function takes two arguments: the IP address and the port of the proxy server.
@@ -31,6 +63,11 @@ To ensure that the system-wide proxy settings are unset upon program termination
 
 	defer UnsetProxy()
 
-after the `SetProxy` call.
+after the SetProxy call.
+
+[here]: https://keith.github.io/xcode-man-pages/networksetup.8.html
+[gsettings]: https://github.com/GNOME/gsettings-desktop-schemas/blob/master/schemas/org.gnome.system.proxy.gschema.xml.in
+[configuration]: https://developer-old.gnome.org/ProxyConfiguration/
+[InternetSetOptionW]: https://learn.microsoft.com/en-us/windows/win32/api/wininet/nf-wininet-internetsetoptionw
 */
 package sysproxy
