9bc31aae074b8a0ef75a63c9e407c9116315ae13
amir gh <amir@we-pn.com>
2024-03-21 14:29:32 -0700

    using number instead of const where applicable

--- transport/socks5/stream_dialer.go
+++ transport/socks5/stream_dialer.go
@@ -23,26 +23,6 @@ import (
 	"github.com/Jigsaw-Code/outline-sdk/transport"
 )
 
-// bufferSize: The maximum buffer size is
-// 3 (1 socks version + 1 method selection + 1 methods)
-// + 1 (auth version) + 1 (username length) + 255 (username) + 1 (password length) + 255 (password)
-const (
-	maxCredentialLength = 255
-	minCredentialLength = 1
-	socksProtocolVer    = 0x05
-	noAuthMethod        = 0x00
-	userPassAuthMethod  = 0x02
-	numberOfAuthMethods = 0x01
-	authVersion         = 0x01
-	connectCommand      = 0x01
-	rsv                 = 0x00
-	authSuccess         = 0x00
-	connectSuccess      = 0x00
-	addrLengthIPv4      = 4
-	addrLengthIPv6      = 16
-	bufferSize          = (1 + 1 + 1) + (1 + 1 + 255 + 1 + 255) + 256
-)
-
 // https://datatracker.ietf.org/doc/html/rfc1929
 // Credentials can be nil, and that means no authentication.
 type Credentials struct {
@@ -52,39 +32,26 @@ type Credentials struct {
 
 func NewCredentials(username, password string) (Credentials, error) {
 	var c Credentials
-	if err := c.setUsername(username); err != nil {
-		return c, err
-	}
-	if err := c.setPassword(password); err != nil {
-		return c, err
-	}
-	return c, nil
-}
-
-// SetUsername sets the username field, ensuring it doesn't exceed 255 bytes in length and is at least 1 byte.
-func (c *Credentials) setUsername(username string) error {
 	usernameBytes := []byte(username)
-	if len(usernameBytes) > maxCredentialLength {
-		return errors.New("username exceeds 255 bytes")
+	passwordBytes := []byte(password)
+
+	if len(usernameBytes) > 255 {
+		return c, errors.New("username exceeds 255 bytes")
 	}
-	if len(usernameBytes) < minCredentialLength {
-		return errors.New("username must be at least 1 byte")
+	if len(usernameBytes) == 0 {
+		return c, errors.New("username must be at least 1 byte")
 	}
 	c.username = usernameBytes
-	return nil
-}
 
-// SetPassword sets the password field, ensuring it doesn't exceed 255 bytes in length and is at least 1 byte.
-func (c *Credentials) setPassword(password string) error {
-	passwordBytes := []byte(password)
-	if len(passwordBytes) > maxCredentialLength {
-		return errors.New("password exceeds 255 bytes")
+	if len(passwordBytes) > 255 {
+		return c, errors.New("password exceeds 255 bytes")
 	}
-	if len(passwordBytes) < minCredentialLength {
-		return errors.New("password must be at least 1 byte")
+	if len(passwordBytes) == 0 {
+		return c, errors.New("password must be at least 1 byte")
 	}
 	c.password = passwordBytes
-	return nil
+
+	return c, nil
 }
 
 // NewStreamDialer creates a [transport.StreamDialer] that routes connections to a SOCKS5
@@ -123,7 +90,11 @@ func (c *streamDialer) DialStream(ctx context.Context, remoteAddr string) (trans
 	// For protocol details, see https://datatracker.ietf.org/doc/html/rfc1928#section-3
 	// Creating a single buffer for method selection, authentication, and connection request
 	// Buffer large enough for method, auth, and connect requests with a domain name address.
-	var buffer [bufferSize]byte
+	// The maximum buffer size is:
+	// 3 (1 socks version + 1 method selection + 1 methods)
+	// + 1 (auth version) + 1 (username length) + 255 (username) + 1 (password length) + 255 (password)
+	// + 256 (max domain name length)
+	var buffer [(1 + 1 + 1) + (1 + 1 + 255 + 1 + 255) + 256]byte
 	var b []byte
 
 	if c.credentials == nil {
@@ -133,11 +104,11 @@ func (c *streamDialer) DialStream(ctx context.Context, remoteAddr string) (trans
 		// +----+----------+----------+
 		// | 1  |    1     | 1 to 255 |
 		// +----+----------+----------+
-		b = append(buffer[:0], socksProtocolVer, numberOfAuthMethods, noAuthMethod)
+		b = append(buffer[:0], 5, 1, 0)
 	} else {
 		// https://datatracker.ietf.org/doc/html/rfc1929
 		// Method selection part: VER = 5, NMETHODS = 1, METHODS = 2 (username/password)
-		b = append(buffer[:0], socksProtocolVer, numberOfAuthMethods, userPassAuthMethod)
+		b = append(buffer[:0], 5, 1, authMethodUserPass)
 
 		// Authentication part: VER = 1, ULEN = 1, UNAME = 1~255, PLEN = 1, PASSWD = 1~255
 		// +----+------+----------+------+----------+
@@ -145,7 +116,7 @@ func (c *streamDialer) DialStream(ctx context.Context, remoteAddr string) (trans
 		// +----+------+----------+------+----------+
 		// | 1  |  1   | 1 to 255 |  1   | 1 to 255 |
 		// +----+------+----------+------+----------+
-		b = append(b, authVersion)
+		b = append(b, 1)
 		b = append(b, byte(len(c.credentials.username)))
 		b = append(b, c.credentials.username...)
 		b = append(b, byte(len(c.credentials.password)))
@@ -159,7 +130,7 @@ func (c *streamDialer) DialStream(ctx context.Context, remoteAddr string) (trans
 	// +----+-----+-------+------+----------+----------+
 	// | 1  |  1  | X'00' |  1   | Variable |    2     |
 	// +----+-----+-------+------+----------+----------+
-	b = append(b, socksProtocolVer, connectCommand, rsv)
+	b = append(b, 5, 1, 0)
 	// TODO: Probably more memory efficient if remoteAddr is added to the buffer directly.
 	b, err = appendSOCKS5Address(b, remoteAddr)
 	if err != nil {
@@ -184,16 +155,16 @@ func (c *streamDialer) DialStream(ctx context.Context, remoteAddr string) (trans
 	// buffer[0]: VER, buffer[1]: METHOD
 	// Reuse buffer for better performance.
 	if _, err = io.ReadFull(proxyConn, buffer[:2]); err != nil {
-		return nil, fmt.Errorf("failed to read method server response")
+		return nil, fmt.Errorf("failed to read method server response: %w", err)
 	}
-	if buffer[0] != socksProtocolVer {
+	if buffer[0] != 5 {
 		return nil, fmt.Errorf("invalid protocol version %v. Expected 5", buffer[0])
 	}
 
 	switch buffer[1] {
-	case noAuthMethod:
+	case authMethodNoAuth:
 		// No authentication required.
-	case userPassAuthMethod:
+	case authMethodUserPass:
 		// 2. Read authentication version and status
 		// VER = 1, STATUS = 0
 		// +----+--------+
@@ -206,10 +177,10 @@ func (c *streamDialer) DialStream(ctx context.Context, remoteAddr string) (trans
 		if _, err = io.ReadFull(proxyConn, buffer[2:4]); err != nil {
 			return nil, fmt.Errorf("failed to read authentication version and status: %w", err)
 		}
-		if buffer[2] != authVersion {
+		if buffer[2] != 1 {
 			return nil, fmt.Errorf("invalid authentication version %v. Expected 1", buffer[2])
 		}
-		if buffer[3] != authSuccess {
+		if buffer[3] != 0 {
 			return nil, fmt.Errorf("authentication failed: %v", buffer[3])
 		}
 	default:
@@ -228,15 +199,15 @@ func (c *streamDialer) DialStream(ctx context.Context, remoteAddr string) (trans
 	// buffer[2]: RSV
 	// buffer[3]: ATYP
 	if _, err = io.ReadFull(proxyConn, buffer[:4]); err != nil {
-		fmt.Printf("failed to read connect server response: %v", err)
 		return nil, fmt.Errorf("failed to read connect server response: %w", err)
 	}
 
-	if buffer[0] != socksProtocolVer {
+	if buffer[0] != 5 {
 		return nil, fmt.Errorf("invalid protocol version %v. Expected 5", buffer[0])
 	}
 
-	if buffer[1] != connectSuccess {
+	// if REP is not 0, it means the server returned an error.
+	if buffer[1] != 0 {
 		return nil, ReplyCode(buffer[1])
 	}
 
@@ -244,9 +215,9 @@ func (c *streamDialer) DialStream(ctx context.Context, remoteAddr string) (trans
 	var bndAddrLen int
 	switch buffer[3] {
 	case addrTypeIPv4:
-		bndAddrLen = addrLengthIPv4
+		bndAddrLen = 4
 	case addrTypeIPv6:
-		bndAddrLen = addrLengthIPv6
+		bndAddrLen = 16
 	case addrTypeDomainName:
 		// buffer[8]: length of the domain name
 		_, err := io.ReadFull(proxyConn, buffer[:1])
