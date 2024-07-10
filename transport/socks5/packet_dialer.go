f0147d2881ffc35238630ff7119409c5ac8db906
amir gh <amir@we-pn.com>
2024-07-10 15:40:17 -0700

    code review improvements

--- transport/socks5/packet_dialer.go
+++ transport/socks5/packet_dialer.go
@@ -16,8 +16,10 @@ package socks5
 
 import (
 	"context"
+	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	"time"
 
@@ -33,7 +35,6 @@ type packetConn struct {
 var _ net.Conn = (*packetConn)(nil)
 
 func (c *packetConn) LocalAddr() net.Addr {
-	// TODO: Is this right?
 	return c.pc.LocalAddr()
 }
 
@@ -54,41 +55,50 @@ func (c *packetConn) SetWriteDeadline(t time.Time) error {
 }
 
 func (c *packetConn) Read(b []byte) (int, error) {
-	// TODO: read header
 	buffer := make([]byte, 65536) // Maximum size for UDP packet
 	n, err := c.pc.Read(buffer)
 	if err != nil {
 		return 0, err
 	}
 	// Minimum size of header is 10 bytes
+	// 2 bytes for reserved, 1 byte for fragment, 1 byte for address type, 4 byte for ipv4, 2 bytes for port
 	if n < 10 {
 		return 0, fmt.Errorf("invalid SOCKS5 UDP packet: too short")
 	}
 
+	pkt := buffer[:n]
+
 	// Start parsing the header
-	rsv := buffer[:2]
+	rsv := pkt[:2]
 	if rsv[0] != 0x00 || rsv[1] != 0x00 {
 		return 0, fmt.Errorf("invalid reserved bytes: expected 0x0000, got %#x%#x", rsv[0], rsv[1])
 	}
 
-	frag := buffer[2]
+	frag := pkt[2]
 	if frag != 0 {
-		return 0, fmt.Errorf("fragmentation is not supported")
+		return 0, errors.New("fragmentation is not supported")
 	}
 
-	atyp := buffer[3]
+	atyp := pkt[3]
 	addrLen := 0
 	switch atyp {
 	case addrTypeIPv4:
-		addrLen = net.IPv4len
+		addrLen = 4
 	case addrTypeIPv6:
-		addrLen = net.IPv6len
+		addrLen = 16
 	case addrTypeDomainName:
 		// Domain name's first byte is the length of the name
-		addrLen = int(buffer[4]) + 1 // +1 for the length byte itself
+		addrLen = int(pkt[4]) + 1 // +1 for the length byte itself
 	default:
 		return 0, fmt.Errorf("unknown address type %#x", atyp)
 	}
+
+	pkt = pkt[4:] // Skip the header
+	addr := pkt[:addrLen]
+
+	pkt = pkt[addrLen:] // Skip the address
+	port := binary.BigEndian.Uint16(pkt[:2])
+	fmt.Printf("Received packet from %d:%d\n", addr, port)
 
 	// Calculate the start position of the actual data
 	headerLength := 4 + addrLen + 2 // RSV (2) + FRAG (1) + ATYP (1) + ADDR (variable) + PORT (2)
@@ -99,8 +109,7 @@ func (c *packetConn) Read(b []byte) (int, error) {
 	// Copy the payload into the provided buffer
 	payloadLength := n - headerLength
 	if payloadLength > len(b) {
-		// maybe raise an error to indicate that the provided buffer is too small?
-		payloadLength = len(b)
+		return 0, io.ErrShortBuffer
 	}
 	copy(b, buffer[headerLength:n])
 
@@ -109,12 +118,14 @@ func (c *packetConn) Read(b []byte) (int, error) {
 
 func (c *packetConn) Write(b []byte) (int, error) {
 	// Encapsulate the payload in a SOCKS5 UDP packet
-	header := []byte{
+	// this is the minimum preallocated header size (10 bytes)
+	header := make([]byte, 10)
+	header = append(header[:0],
 		0x00, 0x00, // Reserved
 		0x00, // Fragment number
-		// To be appended below: ATYP, IPv4, IPv6, Domain name
-		// To be appended below: IP and port (destination address)
-	}
+		// To be appended below:
+		// ATYP, IPv4, IPv6, Domain Name, Port
+	)
 	header, err := appendSOCKS5Address(header, c.dstAddr.String())
 	if err != nil {
 		return 0, fmt.Errorf("failed to append SOCKS5 address: %w", err)
@@ -164,3 +175,42 @@ func (d *Dialer) DialPacket(ctx context.Context, dstAddr string) (net.Conn, erro
 
 	return &packetConn{netDstAddr, pc, sc}, nil
 }
+
+// func readAddress(conn *net.Conn) (string, error) {
+// 	// Read the address type
+// 	fmt.Println("Reading address type")
+// 	addrType := make([]byte, 1)
+// 	addrLen := 0
+// 	n, err := io.ReadFull(*conn, addrType)
+// 	//n, err := *conn.Read(addrType)
+// 	if err != nil {
+// 		return "", fmt.Errorf("failed to read address type: %w", err)
+// 	}
+// 	fmt.Printf("Read address type %d bytes\n", n)
+// 	// Read the address type
+// 	switch addrType[0] {
+// 	case addrTypeIPv4:
+// 		addrLen = 4
+// 	case addrTypeIPv6:
+// 		addrLen = 16
+// 	case addrTypeDomainName:
+// 		// Domain name's first byte is the length of the name
+// 		domainAddrLen := make([]byte, 1)
+// 		_, err := io.ReadFull(*conn, domainAddrLen)
+// 		//_, err := reader.Read(domainAddrLen)
+// 		if err != nil {
+// 			return "", fmt.Errorf("failed to read domain address length: %w", err)
+// 		}
+// 		addrLen = int(domainAddrLen[0])
+// 	default:
+// 		return "", fmt.Errorf("unknown address type %#x", addrType[0])
+// 	}
+// 	fmt.Printf("Address length is: %d\n", addrLen)
+// 	addr := make([]byte, addrLen)
+// 	_, err = io.ReadFull(*conn, addr)
+// 	//_, err = reader.Read(addr)
+// 	if err != nil {
+// 		return "", fmt.Errorf("failed to read address: %w", err)
+// 	}
+// 	return string(addr), nil
+// }
