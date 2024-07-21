ae97fd78c185ad74814fa2218cb5dfd9bdaa77d3
amir gh <amir@we-pn.com>
2024-07-20 19:09:31 -0600

    testing Read Address

--- transport/socks5/socks5_test.go
+++ transport/socks5/socks5_test.go
@@ -15,11 +15,98 @@
 package socks5
 
 import (
+	"bytes"
+	"net"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestReadAddr(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   []byte
+		want    *address
+		wantErr bool
+	}{
+
+		{
+			name:    "IPv4 Example",
+			input:   append([]byte{addrTypeIPv4}, append(net.IPv4(192, 168, 1, 1).To4(), []byte{0x01, 0xF4}...)...),
+			want:    &address{IP: net.IPv4(192, 168, 1, 1), Port: 500},
+			wantErr: false,
+		},
+		{
+			name:    "IPv6 Full",
+			input:   append([]byte{addrTypeIPv6}, append(net.ParseIP("2001:db8::1").To16(), []byte{0x04, 0xD2}...)...),
+			want:    &address{IP: net.ParseIP("2001:db8::1"), Port: 1234},
+			wantErr: false,
+		},
+		{
+			name:    "IPv6 Compressed",
+			input:   append([]byte{addrTypeIPv6}, append(net.ParseIP("fe80::204:61ff:fe9d:f156").To16(), []byte{0x00, 0x50}...)...),
+			want:    &address{IP: net.ParseIP("fe80::204:61ff:fe9d:f156"), Port: 80},
+			wantErr: false,
+		},
+		{
+			name:    "IPv6 Loopback",
+			input:   append([]byte{addrTypeIPv6}, append(net.IPv6loopback.To16(), []byte{0x1F, 0x90}...)...),
+			want:    &address{IP: net.IPv6loopback, Port: 8080},
+			wantErr: false,
+		},
+		{
+			name:    "Domain Short",
+			input:   append([]byte{addrTypeDomainName, 0x0b}, append([]byte("example.com"), []byte{0x23, 0x28}...)...),
+			want:    &address{Name: "example.com", Port: 9000},
+			wantErr: false,
+		},
+		{
+			name:    "Domain Long",
+			input:   append([]byte{addrTypeDomainName, 0x3B}, append([]byte("very-long-domain-name-used-for-testing-purposes.example.com"), []byte{0x00, 0x50}...)...),
+			want:    &address{Name: "very-long-domain-name-used-for-testing-purposes.example.com", Port: 80},
+			wantErr: false,
+		},
+		{
+			name:    "Unrecognized Address Type",
+			input:   []byte{0x00},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "Short Input",
+			input:   []byte{addrTypeIPv4},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := bytes.NewReader(tt.input)
+			got, err := readAddr(r)
+			if tt.wantErr {
+				require.Error(t, err, "Expected an error but got none")
+			} else {
+				require.NoError(t, err, "Did not expect an error but got one")
+			}
+
+			if !tt.wantErr && !compareAddresses(got, tt.want) {
+				t.Errorf("readAddr() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func compareAddresses(a1, a2 *address) bool {
+	if a1 == nil || a2 == nil {
+		return a1 == a2
+	}
+	if a1.IP != nil && !a1.IP.Equal(a2.IP) || a1.Name != a2.Name || a1.Port != a2.Port {
+		return false
+	}
+	return true
+}
 
 func TestAppendSOCKS5Address_IPv4(t *testing.T) {
 	b := []byte{}
