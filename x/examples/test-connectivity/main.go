99a2e2f9e8ef6973af7d491e967b1b38c2c0b11c
amir gh <amir@we-pn.com>
2024-09-03 00:57:53 -0700

    restructuring connectivity report

--- x/examples/test-connectivity/main.go
+++ x/examples/test-connectivity/main.go
@@ -16,7 +16,6 @@ package main
 
 import (
 	"context"
-	ctls "crypto/tls"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -25,7 +24,6 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"net/http/httptrace"
 	"net/url"
 	"os"
 	"path"
@@ -33,38 +31,122 @@ import (
 	"time"
 
 	"github.com/Jigsaw-Code/outline-sdk/dns"
-	"github.com/Jigsaw-Code/outline-sdk/transport/socks5"
-	"github.com/Jigsaw-Code/outline-sdk/transport/tls"
 	"github.com/Jigsaw-Code/outline-sdk/x/config"
 	"github.com/Jigsaw-Code/outline-sdk/x/connectivity"
 	"github.com/Jigsaw-Code/outline-sdk/x/report"
-	"golang.org/x/net/dns/dnsmessage"
 )
 
 var debugLog log.Logger = *log.New(io.Discard, "", 0)
 
 // var errorLog log.Logger = *log.New(os.Stderr, "[ERROR] ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 
-type connectivityReport struct {
-	// Inputs
-	Resolver string `json:"resolver"`
-	Proto    string `json:"proto"`
-	// TODO(fortuna): add sanitized transport config.
-	Transport string `json:"transport"`
+type JSONConnectivityEvents struct {
+	ConnectInfo   []*JSONConnectInfo   `json:"connect_info,omitempty"`
+	DnsInfo       []*JSONDNSInfo       `json:"dns_info,omitempty"`
+	SystemDNSInfo []*JSONSystemDNSInfo `json:"system_dns_info,omitempty"`
+}
 
-	// Observations
-	Time       time.Time  `json:"time"`
-	DurationMs int64      `json:"duration_ms"`
-	Error      *errorJSON `json:"error"`
+type JSONConnectInfo struct {
+	Network   string        `json:"network"`
+	IP        string        `json:"ip"`
+	Port      string        `json:"port"`
+	Error     string        `json:"error,omitempty"`
+	StartTime time.Time     `json:"start_time"`
+	Duration  time.Duration `json:"duration_ms"`
+	ConnError *errorJSON    `json:"conn_error,omitempty"`
+}
+
+type JSONDNSInfo struct {
+	Host         string        `json:"host"`
+	Resolver     string        `json:"resolver"`
+	Network      string        `json:"network"`
+	ResolverType string        `json:"resolver_type"`
+	IPs          []string      `json:"ips"`
+	RSCodes      []string      `json:"rs_codes"`
+	Error        string        `json:"error,omitempty"`
+	StartTime    time.Time     `json:"start_time"`
+	Duration     time.Duration `json:"duration_ms"`
+	ConnError    *errorJSON    `json:"conn_error,omitempty"`
+}
+
+type JSONSystemDNSInfo struct {
+	Host      string        `json:"host"`
+	IPs       []string      `json:"ips"`
+	Error     string        `json:"error,omitempty"`
+	StartTime time.Time     `json:"start_time"`
+	Duration  time.Duration `json:"duration_ms"`
 }
 
 type errorJSON struct {
-	// TODO: add Shadowsocks/Transport error
-	Op string `json:"op,omitempty"`
-	// Posix error, when available
+	Op         string `json:"op,omitempty"`
 	PosixError string `json:"posix_error,omitempty"`
-	// TODO: remove IP addresses
-	Msg string `json:"msg,omitempty"`
+	Msg        string `json:"msg,omitempty"`
+}
+
+// Function to convert ConnectivityEvents to JSON-friendly structure
+func ConvertToJSONFriendly(events *connectivity.ConnectivityEvents) *JSONConnectivityEvents {
+	jsonEvents := &JSONConnectivityEvents{
+		ConnectInfo:   make([]*JSONConnectInfo, len(events.ConnectInfo)),
+		DnsInfo:       make([]*JSONDNSInfo, len(events.DnsInfo)),
+		SystemDNSInfo: make([]*JSONSystemDNSInfo, len(events.SystemDNSInfo)),
+	}
+
+	for i, ci := range events.ConnectInfo {
+		jsonEvents.ConnectInfo[i] = &JSONConnectInfo{
+			Network:   ci.Network,
+			IP:        ci.IP,
+			Port:      ci.Port,
+			StartTime: ci.StartTime,
+			Duration:  time.Duration(ci.Duration.Milliseconds()),
+			ConnError: makeErrorRecord(ci.ConnError),
+		}
+		if ci.Error != nil {
+			jsonEvents.ConnectInfo[i].Error = ci.Error.Error()
+		}
+	}
+
+	for i, di := range events.DnsInfo {
+		ips := make([]string, len(di.IPs))
+		for j, ip := range di.IPs {
+			ips[j] = ip.IP.String()
+		}
+		rsCodes := make([]string, len(di.RSCodes))
+		for j, code := range di.RSCodes {
+			rsCodes[j] = code.String()
+		}
+		jsonEvents.DnsInfo[i] = &JSONDNSInfo{
+			Host:         di.Host,
+			Resolver:     di.Resolver,
+			Network:      di.Network,
+			ResolverType: di.ResolverType,
+			IPs:          ips,
+			RSCodes:      rsCodes,
+			StartTime:    di.StartTime,
+			Duration:     time.Duration(di.Duration.Milliseconds()),
+			ConnError:    makeErrorRecord(di.ConnError),
+		}
+		if di.Error != nil {
+			jsonEvents.DnsInfo[i].Error = di.Error.Error()
+		}
+	}
+
+	for i, sdi := range events.SystemDNSInfo {
+		ips := make([]string, len(sdi.IPs))
+		for j, ip := range sdi.IPs {
+			ips[j] = ip.IP.String()
+		}
+		jsonEvents.SystemDNSInfo[i] = &JSONSystemDNSInfo{
+			Host:      sdi.Host,
+			IPs:       ips,
+			StartTime: sdi.StartTime,
+			Duration:  time.Duration(sdi.Duration.Milliseconds()),
+		}
+		if sdi.Error != nil {
+			jsonEvents.SystemDNSInfo[i].Error = sdi.Error.Error()
+		}
+	}
+
+	return jsonEvents
 }
 
 func makeErrorRecord(result *connectivity.ConnectivityError) *errorJSON {
@@ -88,13 +170,13 @@ func unwrapAll(err error) error {
 	}
 }
 
-func (r connectivityReport) IsSuccess() bool {
-	if r.Error == nil {
-		return true
-	} else {
-		return false
-	}
-}
+// func (r connectivityReport) IsSuccess() bool {
+// 	if r.Error == nil {
+// 		return true
+// 	} else {
+// 		return false
+// 	}
+// }
 
 func init() {
 	flag.Usage = func() {
@@ -170,7 +252,7 @@ func main() {
 	jsonEncoder := json.NewEncoder(os.Stdout)
 	jsonEncoder.SetEscapeHTML(false)
 	configToDialer := config.NewDefaultConfigToDialer()
-	ctx := SetupConnectivityTrace(context.Background())
+	ctx, connectivityEvents := connectivity.SetupConnectivityTrace(context.Background())
 	for _, resolverHost := range strings.Split(*resolverFlag, ",") {
 		resolverHost := strings.TrimSpace(resolverHost)
 		var result *connectivity.ConnectivityError
@@ -178,11 +260,8 @@ func main() {
 		for _, testType := range strings.Split(*testTypeFlag, ",") {
 			testType = strings.TrimSpace(testType)
 			var resolver dns.Resolver
-			var Protocol string
-			startTime := time.Now()
 			switch testType {
 			case "do53-tcp":
-				Protocol = "tcp"
 				streamDialer, err := configToDialer.NewStreamDialer(*transportFlag)
 				if err != nil {
 					log.Fatalf("Failed to create StreamDialer: %v", err)
@@ -194,7 +273,6 @@ func main() {
 					log.Fatalf("Connectivity test failed to run: %v", err)
 				}
 			case "do53-udp":
-				Protocol = "udp"
 				packetDialer, err := configToDialer.NewPacketDialer(*transportFlag)
 				if err != nil {
 					log.Fatalf("Failed to create PacketDialer: %v", err)
@@ -206,7 +284,6 @@ func main() {
 					log.Fatalf("Connectivity test failed to run: %v", err)
 				}
 			case "doh":
-				Protocol = "tcp"
 				streamDialer, err := configToDialer.NewStreamDialer(*transportFlag)
 				if err != nil {
 					log.Fatalf("Failed to create StreamDialer: %v", err)
@@ -218,7 +295,6 @@ func main() {
 					log.Fatalf("Connectivity test failed to run: %v", err)
 				}
 			case "dot":
-				Protocol = "tcp"
 				streamDialer, err := configToDialer.NewStreamDialer(*transportFlag)
 				if err != nil {
 					log.Fatalf("Failed to create StreamDialer: %v", err)
@@ -230,7 +306,6 @@ func main() {
 					log.Fatalf("Connectivity test failed to run: %v", err)
 				}
 			case "http":
-				Protocol = "tcp"
 				streamDialer, err := configToDialer.NewStreamDialer(*transportFlag)
 				if err != nil {
 					log.Fatalf("Failed to create StreamDialer: %v", err)
@@ -240,7 +315,6 @@ func main() {
 					log.Fatalf("Connectivity test failed to run: %v", err)
 				}
 			case "http3":
-				Protocol = "udp"
 				packetDialer, err := configToDialer.NewPacketDialer(*transportFlag)
 				if err != nil {
 					log.Fatalf("Failed to create PacketDialer: %v", err)
@@ -252,26 +326,20 @@ func main() {
 			default:
 				log.Fatalf(`Invalid Test Type %v.`, testType)
 			}
-			testDuration := time.Since(startTime)
 			if result == nil {
 				success = true
 			}
 			debugLog.Printf("Test Type: %v Resolver Address: %v domain: %v result: %v", testType, resolverAddress, *domainFlag, result)
-			sanitizedConfig, err := config.SanitizeConfig(*transportFlag)
-			if err != nil {
-				log.Fatalf("Failed to sanitize config: %v", err)
-			}
-			var r report.Report = connectivityReport{
-				Resolver: resolverAddress,
-				Proto:    Protocol, // change this
-				Time:     startTime.UTC().Truncate(time.Second),
-				// TODO(fortuna): Add sanitized config:
-				Transport:  sanitizedConfig,
-				DurationMs: testDuration.Milliseconds(),
-				Error:      makeErrorRecord(result),
-			}
+			//sanitizedConfig, err := config.SanitizeConfig(*transportFlag)
+			// if err != nil {
+			// 	log.Fatalf("Failed to sanitize config: %v", err)
+			// }
+
+			fmt.Printf("Connectivity Events: %+v\n", connectivityEvents)
+			jsonFriendlyEvents := ConvertToJSONFriendly(connectivityEvents)
+			var r report.Report = jsonFriendlyEvents
 			if reportCollector != nil {
-				err = reportCollector.Collect(context.Background(), r)
+				err := reportCollector.Collect(context.Background(), r)
 				if err != nil {
 					debugLog.Printf("Failed to collect report: %v\n", err)
 				}
@@ -281,138 +349,4 @@ func main() {
 			os.Exit(1)
 		}
 	}
-}
-
-func SetupConnectivityTrace(ctx context.Context) context.Context {
-	t := &dns.DNSClientTrace{
-		QuestionSent: func(question dnsmessage.Question) {
-			fmt.Println("DNS query started for", question.Name.String())
-		},
-		ResponsDone: func(question dnsmessage.Question, msg *dnsmessage.Message, err error) {
-			if err != nil {
-				fmt.Printf("DNS query for %s failed: %v\n", question.Name.String(), err)
-			} else {
-				// Prepare to collect IP addresses
-				var ips []string
-
-				// Iterate over the answer section
-				for _, answer := range msg.Answers {
-					switch rr := answer.Body.(type) {
-					case *dnsmessage.AResource:
-						// Handle IPv4 addresses - convert [4]byte to IP string
-						ipv4 := net.IP(rr.A[:]) // Convert [4]byte to net.IP
-						ips = append(ips, ipv4.String())
-					case *dnsmessage.AAAAResource:
-						// Handle IPv6 addresses - convert [16]byte to IP string
-						ipv6 := net.IP(rr.AAAA[:]) // Convert [16]byte to net.IP
-						ips = append(ips, ipv6.String())
-					}
-				}
-
-				// Print all resolved IP addresses
-				if len(ips) > 0 {
-					fmt.Printf("Resolved IPs for %s: %v\n", question.Name.String(), ips)
-				} else {
-					fmt.Printf("No IPs found for %s\n", question.Name.String())
-				}
-			}
-		},
-		ConnectDone: func(network, addr string, err error) {
-			if err != nil {
-				fmt.Printf("%v Connection to %s failed: %v\n", network, addr, err)
-			} else {
-				fmt.Printf("%v Connection to %s succeeded\n", network, addr)
-			}
-		},
-		WroteDone: func(err error) {
-			if err != nil {
-				fmt.Printf("Write failed: %v\n", err)
-			} else {
-				fmt.Println("Write succeeded")
-			}
-		},
-		ReadDone: func(err error) {
-			if err != nil {
-				fmt.Printf("Read failed: %v\n", err)
-			} else {
-				fmt.Println("Read succeeded")
-			}
-		},
-	}
-
-	// Variables to store the timestamps
-	var startTLS time.Time
-
-	ht := &httptrace.ClientTrace{
-		DNSStart: func(info httptrace.DNSStartInfo) {
-			fmt.Printf("DNS start: %v\n", info)
-		},
-		DNSDone: func(info httptrace.DNSDoneInfo) {
-			fmt.Printf("DNS done: %v\n", info)
-		},
-		ConnectStart: func(network, addr string) {
-			fmt.Printf("Connect start: %v %v\n", network, addr)
-		},
-		ConnectDone: func(network, addr string, err error) {
-			fmt.Printf("Connect done: %v %v %v\n", network, addr, err)
-		},
-		GotFirstResponseByte: func() {
-			fmt.Println("Got first response byte")
-		},
-		WroteHeaderField: func(key string, value []string) {
-			fmt.Printf("Wrote header field: %v %v\n", key, value)
-		},
-		WroteHeaders: func() {
-			fmt.Println("Wrote headers")
-		},
-		WroteRequest: func(info httptrace.WroteRequestInfo) {
-			fmt.Printf("Wrote request: %v\n", info)
-		},
-		TLSHandshakeStart: func() {
-			startTLS = time.Now()
-		},
-		TLSHandshakeDone: func(state ctls.ConnectionState, err error) {
-			if err != nil {
-				fmt.Printf("TLS handshake failed: %v\n", err)
-			}
-			fmt.Printf("SNI: %v\n", state.ServerName)
-			fmt.Printf("TLS version: %v\n", state.Version)
-			fmt.Printf("ALPN: %v\n", state.NegotiatedProtocol)
-			fmt.Printf("TLS handshake took %v seconds.\n", time.Since(startTLS).Seconds())
-		},
-	}
-
-	tlsTrace := &tls.TLSClientTrace{
-		TLSHandshakeStart: func() {
-			fmt.Println("TLS handshake started")
-			startTLS = time.Now()
-		},
-		TLSHandshakeDone: func(state ctls.ConnectionState, err error) {
-			if err != nil {
-				fmt.Printf("TLS handshake failed: %v\n", err)
-			}
-			fmt.Printf("SNI: %v\n", state.ServerName)
-			fmt.Printf("TLS version: %v\n", state.Version)
-			fmt.Printf("ALPN: %v\n", state.NegotiatedProtocol)
-			fmt.Printf("TLS handshake took %v seconds.\n", time.Since(startTLS).Seconds())
-		},
-	}
-
-	socksTrace := &socks5.SOCKS5ClientTrace{
-		RequestStarted: func(cmd byte, dstAddr string) {
-			fmt.Printf("SOCKS5 request started: cmd: %v address: %v\n", cmd, dstAddr)
-		},
-		RequestDone: func(network string, bindAddr string, err error) {
-			if err != nil {
-				fmt.Printf("SOCKS5 request failed! network: %v, bindAddr: %v, error: %v \n", network, bindAddr, err)
-			}
-			fmt.Printf("SOCKS5 request succeeded! network: %v, bindAddr: %v \n", network, bindAddr)
-		},
-	}
-
-	ctx = httptrace.WithClientTrace(ctx, ht)
-	ctx = dns.WithDNSClientTrace(ctx, t)
-	ctx = tls.WithTLSClientTrace(ctx, tlsTrace)
-	ctx = socks5.WithSOCKS5ClientTrace(ctx, socksTrace)
-	return ctx
 }
