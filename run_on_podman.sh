ef2c9f2935c6846fdbbc59c33f45601548c82038
Vinicius Fortuna <fortuna@users.noreply.github.com>
2024-10-23 17:27:59 -0400

    Update Copyright line

--- run_on_podman.sh
+++ run_on_podman.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright 2023 Jigsaw Operations LLC
+# Copyright 2023 The Outline Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
