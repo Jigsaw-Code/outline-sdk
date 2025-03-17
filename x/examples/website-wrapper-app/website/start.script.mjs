0d0130c07374b80a6bd335addeb054a6c9413135
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2025-03-17 15:25:37 -0700

    site works

--- x/examples/website-wrapper-app/website/start.script.mjs
+++ x/examples/website-wrapper-app/website/start.script.mjs
@@ -3,7 +3,9 @@ import httpProxy from "http-proxy";
 import http from "node:http";
 import { fileURLToPath } from "node:url";
 import { createServer } from "vite";
+import { randomUUID } from "node:crypto";
 
+const NAVIGATION_PATH = `/${randomUUID()}`;
 const TARGET_DOMAIN = process.env.TARGET_DOMAIN || "www.example.com";
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
@@ -19,39 +21,40 @@ const getLocalServerUrl = (server) => {
 
 (async function () {
   const navigationServer = await createServer({
-    root: __dirname
+    root: __dirname,
+    server: {
+      allowedHosts: true
+    }
   });
 
   await navigationServer.listen();
 
-  console.log(`Navigation server: ${getLocalServerUrl(navigationServer.httpServer)}`);
-
   const proxyMiddleware = httpProxy.createProxyServer({});
   const proxyServer = http.createServer((request, response) => {
-    if (request.url.startsWith("/nav")) {
+    if (request.url.startsWith(NAVIGATION_PATH)) {
       return proxyMiddleware.web(request, response, {
         target: getLocalServerUrl(navigationServer.httpServer),
       });
     }
 
     return proxyMiddleware.web(request, response, {
-      target: `http://${TARGET_DOMAIN}`,
+      target: `https://${TARGET_DOMAIN}`,
+      changeOrigin: true
     });
   });
 
   proxyServer.listen(async () => {
-    console.log(`Proxy server: ${getLocalServerUrl(proxyServer)}`);
-
     const listener = await ngrok.forward({
       addr: getLocalServerUrl(proxyServer),
       authtoken_from_env: false,
       authtoken: process.env.NGROK_TOKEN,
+      verify_upstream_tls: false,
       response_header_add: [
         "Allow-Access-Control-Origin: *",
         "Access-Control-Allow-Headers: *",
       ],
     });
 
-    console.log(`TLS frontend URL: ${listener.url()}`);
+    console.log(listener.url() + NAVIGATION_PATH);
   });
 })();
