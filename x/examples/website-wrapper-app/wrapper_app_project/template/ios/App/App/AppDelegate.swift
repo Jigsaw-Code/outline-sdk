1f30ffe428dd8411f67fd722eb3a5abb31b7593b
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2025-04-07 15:26:18 -0400

    ios error handling

--- x/examples/website-wrapper-app/wrapper_app_project/template/ios/App/App/AppDelegate.swift
+++ x/examples/website-wrapper-app/wrapper_app_project/template/ios/App/App/AppDelegate.swift
@@ -1,15 +1,18 @@
-import UIKit
 import Capacitor
 import Mobileproxy
+import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
+
     private var proxy: MobileproxyProxy? = nil
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
 
         self.window?.rootViewController = OutlineBridgeViewController()
 
@@ -24,12 +27,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             MobileproxyNewStderrLogWriter(),
             &dialerError
         ) {
+            if let error = dialerError {
+                self.showError(error)
+                return
+            }
+
             var proxyError: NSError?
             self.proxy = MobileproxyRunProxy(
                 "127.0.0.1:8080",
                 dialer,
                 &proxyError
             )
+
+            if let error = proxyError {
+                self.showError(error)
+
+                self.proxy = nil
+                return
+            }
         }
     }
 
@@ -40,16 +55,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+    func application(
+        _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
         // Called when the app was launched with a url. Feel free to add additional processing here,
         // but if you want the App API to support tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    func application(
+        _ application: UIApplication, continue userActivity: NSUserActivity,
+        restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+    ) -> Bool {
         // Called when the app was launched with an activity, including Universal Links.
         // Feel free to add additional processing here, but if you want the App API to support
         // tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
+        return ApplicationDelegateProxy.shared.application(
+            application, continue: userActivity, restorationHandler: restorationHandler)
+    }
+
+    private func showError(error: NSError) {
+        guard let rootViewController = self.window?.rootViewController else {
+            print("Could not find root view controller to present error.")
+            return
+        }
+
+        let dialog = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+
+        dialog.addAction(UIAlertAction(title: "OK", style: .deault))
+
+        rootViewController.present(alert, animated: false)
     }
 }
