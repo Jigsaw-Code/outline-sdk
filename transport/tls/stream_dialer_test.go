971d40055d646ecd174dbcf832312de64aa398c0
Vinicius Fortuna <fortuna@users.noreply.github.com>
2025-04-16 18:02:32 -0400

    Add test

--- transport/tls/stream_dialer_test.go
+++ transport/tls/stream_dialer_test.go
@@ -16,8 +16,15 @@ package tls
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
 	"crypto/x509"
+	"crypto/x509/pkix"
+	"math/big"
+	"net"
 	"testing"
+	"time"
 
 	"github.com/Jigsaw-Code/outline-sdk/transport"
 	"github.com/stretchr/testify/require"
@@ -175,4 +182,81 @@ func (d *connCounterDialer) DialStream(ctx context.Context, raddr string) (trans
 func (c countedStreamConn) Close() error {
 	c.counter.activeConns--
 	return c.StreamConn.Close()
+}
+
+// Helper function to create a self-signed certificate (Root CA)
+func createRootCA(t *testing.T) (*x509.Certificate, *ecdsa.PrivateKey) {
+	t.Helper()
+	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(t, err)
+
+	template := x509.Certificate{
+		SerialNumber:          big.NewInt(1),
+		Subject:               pkix.Name{Organization: []string{"Test Root CA"}},
+		NotBefore:             time.Now().Add(-1 * time.Hour),
+		NotAfter:              time.Now().Add(24 * time.Hour),
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+	}
+
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &privKey.PublicKey, privKey)
+	require.NoError(t, err)
+
+	cert, err := x509.ParseCertificate(certDER)
+	require.NoError(t, err)
+
+	return cert, privKey
+}
+
+// Helper function to create a leaf certificate signed by a parent
+func createLeafCert(t *testing.T, dnsNames []string, ipAddresses []net.IP, parentCert *x509.Certificate, parentKey *ecdsa.PrivateKey, notBefore, notAfter time.Time) (*x509.Certificate, *ecdsa.PrivateKey) {
+	t.Helper()
+	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(t, err)
+
+	template := x509.Certificate{
+		SerialNumber:          big.NewInt(2),
+		Subject:               pkix.Name{CommonName: dnsNames[0]}, // Use first DNS name as CN
+		DNSNames:              dnsNames,
+		IPAddresses:           ipAddresses,
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}, // Server cert
+		BasicConstraintsValid: true,
+		IsCA:                  false,
+	}
+
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, parentCert, &privKey.PublicKey, parentKey)
+	require.NoError(t, err)
+
+	cert, err := x509.ParseCertificate(certDER)
+	require.NoError(t, err)
+
+	return cert, privKey
+}
+
+func TestGeneratedCert_Valid(t *testing.T) {
+	// 1. Generate Certs
+	rootCA, rootKey := createRootCA(t)
+	leafCert, _ := createLeafCert(t, []string{"test.local"}, nil, rootCA, rootKey, time.Now().Add(-1*time.Hour), time.Now().Add(1*time.Hour))
+
+	// 2. Setup Root Pool for Client
+	rootPool := x509.NewCertPool()
+	rootPool.AddCert(rootCA)
+
+	verificationContext := &CertVerificationContext{PeerCertificates: []*x509.Certificate{leafCert}}
+
+	sysVerifier := &StandardCertVerifier{CertificateName: "test.local"}
+	require.ErrorContains(t, sysVerifier.VerifyCertificate(verificationContext), "not trusted")
+
+	customVerifier := &StandardCertVerifier{CertificateName: "test.local", Roots: rootPool}
+	require.NoError(t, customVerifier.VerifyCertificate(verificationContext))
+
+	wrongDomainVerifier := &StandardCertVerifier{CertificateName: "other.local", Roots: rootPool}
+	var hostErr x509.HostnameError
+	require.ErrorAs(t, wrongDomainVerifier.VerifyCertificate(verificationContext), &hostErr)
+	require.Equal(t, "other.local", hostErr.Host)
+	require.Equal(t, leafCert, hostErr.Certificate)
 }
