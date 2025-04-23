45867af6a2cdfba929edd8f63eba0f38eb5c5a34
Sarah Laplante <laplante@google.com>
2025-04-23 17:18:03 +0200

    split config out into its own file

--- x/psiphon/psiphon_integration_test.go
+++ x/psiphon/psiphon_integration_test.go
@@ -5,18 +5,31 @@ package psiphon
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-// It's useful to test actually starting psiphon connections,
-// but doing so requires supplying a valid psiphon config with private information.
-// To run these tests please supply your own config here.
-var privatePsiphonConfig = `{<YOUR_CONFIG_HERE>}`
+func readPsiphonConfigFromFile(tb testing.TB) (string, error) {
+	configBytes, err := os.ReadFile("integration_test_config.yaml")
+	if err != nil {
+		return "", err
+	}
+	return string(configBytes), nil
+}
 
 func newValidTestConfig(tb testing.TB) (*DialerConfig, func()) {
+	// It's useful to test actually starting psiphon connections,
+	// but doing so requires supplying a valid psiphon config with private information.
+	// To run these tests please supply your own config in integration_test_config.yaml
+	privatePsiphonConfig, err := readPsiphonConfigFromFile(tb)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			tb.Skip("Integration testing for Psiphon requires adding a user-supplied config in integration_test_config.yaml")
+		}
+	}
 	tempDir, err := os.MkdirTemp("", "psiphon")
 	require.NoError(tb, err)
 	return &DialerConfig{
@@ -26,10 +39,6 @@ func newValidTestConfig(tb testing.TB) (*DialerConfig, func()) {
 }
 
 func TestDialer_CancelledAfterStart_DoesntCloseTunnel(t *testing.T) {
-	if privatePsiphonConfig == "{<YOUR_CONFIG_HERE>}" {
-		t.Skip("Integration testing for Psiphon requires adding a user-supplied config")
-	}
-
 	cfg, delete := newValidTestConfig(t)
 	defer delete()
 	startCtx, startCancel := context.WithCancel(context.Background())
