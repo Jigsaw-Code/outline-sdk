5e1d40a4c1dee0d6b23be16f72ad8af86c972573
Marcel Oomens <mail@marceloomens.com>
2025-05-20 09:25:25 +0200

    Refactor build script (#1)
    * WIP: Refactor script configuration
    
    Signed-off-by: Marcel Oomens, First Draft Media <marcel@firstdraft.media>
    
    * WIP: Refactor build script
    
    Modularize reading the build config
    
    Signed-off-by: Marcel Oomens, First Draft Media <marcel@firstdraft.media>
    
    * WIP: Refactor build script
    
    - Split utility functions into a separate util.mjs module
    - Wrap all console output in chalk (since we’re importing it anyway)
    
    Signed-off-by: Marcel Oomens, First Draft Media <marcel@firstdraft.media>
    
    * WIP: refactor
    
    Signed-off-by: Marcel Oomens, First Draft Media <marcel@firstdraft.media>
    
    * Fix parentheses issue
    
    Fix parentheses issue with smartDialerConfig
    
    Signed-off-by: Marcel Oomens, First Draft Media <marcel@firstdraft.media>
    
    ---------
    
    Signed-off-by: Marcel Oomens, First Draft Media <marcel@firstdraft.media>

--- x/examples/website-wrapper-app/wrapper_app_project/.scripts/build.mjs
+++ x/examples/website-wrapper-app/wrapper_app_project/.scripts/build.mjs
@@ -11,253 +11,82 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+import { exec, execFile } from 'node:child_process'
+import { promises as fs } from 'node:fs'
+import path from 'node:path'
+import { pathToFileURL } from 'node:url'
+import { promisify } from 'node:util'
 
-import { exec, execFile } from "node:child_process";
-import { glob } from "glob";
-import { promisify } from "node:util";
-import chalk from "chalk";
-import archiver from "archiver";
-import { promises as fsp } from 'fs';
-import fs from "node:fs";
-import handlebars from "handlebars";
-import path from "node:path";
-import minimist from "minimist";
-import YAML from 'yaml';
+import chalk from 'chalk'
+import { glob } from 'glob'
+import handlebars from 'handlebars'
 
-const OUTPUT_DIR = path.join(process.cwd(), "output");
-const WRAPPER_APP_TEMPLATE_DIR = path.join(
-  process.cwd(),
-  "wrapper_app_project/template",
-);
+const TEMPLATE = path.join(process.cwd(), 'wrapper_app_project/template');
 
-const DEFAULT_SMART_DIALER_CONFIG = {
-  dns: [{
-    https: { name: "9.9.9.9" },
-  }],
-  tls: ["", "split:1", "split:2", "tlsfrag:1"],
-};
+import { getCliConfig, getFileConfig, DEFAULT_CONFIG } from './config.mjs'
+import { resolveConfiguration, zip } from './util.mjs'
 
-const SMART_DIALER_CONFIG = await (async () => {
-  const data = await fsp.readFile('config.yaml', 'utf8')
-    .catch((e) => {
-      if (e?.code === 'ENOENT') { return undefined; }
-      else if (e) { throw new Error(e); }
-    });
-  
-  if (!data) return undefined;
-  
-  const dict = YAML.parse(data);
-  const config = dict?.smartDialerConfig;
-
-  return config ? JSON.stringify(config) : undefined;
-})() ?? JSON.stringify(DEFAULT_SMART_DIALER_CONFIG);
-
-export default async function main(
-  {
-    additionalDomains = [],
-    appId,
-    appName,
-    entryUrl = "https://www.example.com",
-    navigationUrl,
-    output = OUTPUT_DIR,
-    platform,
-    smartDialerConfig = SMART_DIALER_CONFIG,
-  },
-) {
-  const WRAPPER_APP_OUTPUT_DIR = path.resolve(output, "wrapper_app_project");
-  const WRAPPER_APP_OUTPUT_ZIP = path.resolve(
-    output,
-    "wrapper_app_project.zip",
-  );
-
-  const SDK_MOBILEPROXY_OUTPUT_DIR = path.resolve(output, "mobileproxy");
-  const WRAPPER_APP_OUTPUT_SDK_MOBILEPROXY_DIR = path.resolve(
-    WRAPPER_APP_OUTPUT_DIR,
-    "mobileproxy",
-  );
-
-  if (!fs.existsSync(SDK_MOBILEPROXY_OUTPUT_DIR)) {
-    console.log(
-      `Building the Outline SDK mobileproxy library for ${platform}...`,
-    );
-
-    await promisify(execFile)("npm", [
-      "run",
-      "build:mobileproxy",
-      platform,
-      output,
-    ], { shell: false });
-  }
-
-  const sourceFilepaths = await glob(
-    path.join(WRAPPER_APP_TEMPLATE_DIR, "**", "*"),
-    {
-      nodir: true,
-      dot: true,
-    },
-  );
-
-  console.log("Building project from template...");
-
-  let templateArguments;
-
-  try {
-    templateArguments = resolveTemplateArguments(platform, entryUrl, {
-      additionalDomains,
-      appId,
-      appName,
-      navigationUrl,
-      smartDialerConfig,
-    });
-  } catch (cause) {
-    throw new TypeError("Failed to resolve the project template arguments", {
-      cause,
-    });
-  }
-
-  for (const sourceFilepath of sourceFilepaths) {
-    const destinationFilepath = path.join(
-      WRAPPER_APP_OUTPUT_DIR,
-      path.relative(
-        WRAPPER_APP_TEMPLATE_DIR,
-        sourceFilepath,
-      ),
-    );
-
-    // ensure directory
-    fs.mkdirSync(path.dirname(destinationFilepath), { recursive: true });
-
-    if (!sourceFilepath.endsWith(".handlebars")) {
-      fs.copyFileSync(sourceFilepath, destinationFilepath);
-      continue;
-    }
-
-    const template = handlebars.compile(
-      fs.readFileSync(sourceFilepath, "utf8"),
-    );
-
-    fs.writeFileSync(
-      destinationFilepath.replace(/\.handlebars$/, ""),
-      template(templateArguments),
-      "utf8",
-    );
-  }
-
-  console.log("Copying mobileproxy files into the project...");
-
-  fs.cpSync(
-    SDK_MOBILEPROXY_OUTPUT_DIR,
-    WRAPPER_APP_OUTPUT_SDK_MOBILEPROXY_DIR,
-    { recursive: true },
-  );
-
-  console.log("Installing external dependencies for the project...");
-  await promisify(exec)(`
-    cd ${WRAPPER_APP_OUTPUT_DIR.replaceAll(" ", "\\ ")}
-    npm install
-    npx cap sync ${platform}
-  `);
-
-  console.log(`Zipping project to ${chalk.blue(WRAPPER_APP_OUTPUT_ZIP)}...`);
-  await zip(WRAPPER_APP_OUTPUT_DIR, WRAPPER_APP_OUTPUT_ZIP);
-
-  console.log("Project ready!");
+/* See https://stackoverflow.com/questions/57838022/detect-whether-es-module-is-run-from-command-line-in-node*/
+if (import.meta.url !== pathToFileURL(`${process.argv[1]}`).href) {
+  throw new Error('Build script must be run from the cli')
 }
 
-function zip(root, destination) {
-  const job = archiver("zip", { zlib: { level: 9 } });
-  const destinationStream = fs.createWriteStream(destination);
+const config = resolveConfiguration({
+  ...DEFAULT_CONFIG,
+  ...(await getFileConfig('config.yaml')),
+  ...getCliConfig(process.argv)
+})
 
-  return new Promise((resolve, reject) => {
-    job.directory(root, false);
-    job.pipe(destinationStream);
+const WRAPPER_APP_OUTPUT_TARGET = path.resolve(config.output, config.appName)
+const WRAPPER_APP_OUTPUT_ZIP = path.resolve(config.output, `${config.appName}.zip`)
 
-    destinationStream.on("close", resolve);
+const SDK_MOBILEPROXY_OUTPUT_TARGET = path.resolve(config.output, 'mobileproxy')
+const WRAPPER_APP_OUTPUT_SDK_MOBILEPROXY_DIR = path.resolve(WRAPPER_APP_OUTPUT_TARGET, 'mobileproxy')
 
-    job.on("error", reject);
-    destinationStream.on("error", reject);
-
-    job.finalize();
-  });
+try {
+  await fs.access(SDK_MOBILEPROXY_OUTPUT_TARGET, fs.constants.F_OK)
+} catch (err) {
+  console.log(chalk.green(`Building the Outline SDK mobileproxy library for ${config.platform}...`))
+  await promisify(execFile)('npm', ['run', 'build:mobileproxy', config.platform, config.output])
 }
 
-function resolveTemplateArguments(
-  platform,
-  entryUrl,
+const sourceFilepaths = await glob(
+  path.join(TEMPLATE, '**', '*'),
   {
-    appId,
-    appName,
-    navigationUrl,
-    additionalDomains,
-    smartDialerConfig = DEFAULT_SMART_DIALER_CONFIG,
+    nodir: true,
+    dot: true,
   },
-) {
-  const result = {
-    platform,
-    entryUrl,
-    entryDomain: new URL(entryUrl).hostname,
-  };
+)
 
-  if (!appId) {
-    // Infer an app ID from the entry domain by reversing it (e.g. `www.example.com` becomes `com.example.www`)
-    // It must be lower case, and hyphens are not allowed.
-    result.appId = result.entryDomain.replaceAll("-", "")
-      .toLocaleLowerCase().split(".").reverse().join(".");
-  }
+console.log(chalk.green('Building project from template...'))
 
-  if (!appName) {
-    // Infer an app name from the base entry domain part by title casing the root domain:
-    // (e.g. `www.my-example-app.com` becomes "My Example App")
-    const rootDomain = result.entryDomain.split(".").reverse()[1];
+for (const sourceFilepath of sourceFilepaths) {
+  const destinationFilepath = path.join(WRAPPER_APP_OUTPUT_TARGET, path.relative(TEMPLATE, sourceFilepath))
 
-    result.appName = rootDomain.toLocaleLowerCase().replaceAll(
-      /\w[a-z0-9]*[-_]*/g,
-      (match) => {
-        match = match.replace(/[-_]+/, " ");
+  // ensure directory
+  await fs.mkdir(path.dirname(destinationFilepath), { recursive: true })
 
-        return match.charAt(0).toUpperCase() + match.slice(1).toLowerCase();
-      },
-    );
-  }
-
-  if (navigationUrl) {
-    result.entryUrl = navigationUrl;
-    result.entryDomain = new URL(navigationUrl).hostname;
-  }
-
-  if (typeof additionalDomains === "string") {
-    result.additionalDomains = additionalDomains.split(/,\s*/);
-    result.domainList = [result.entryDomain, ...result.additionalDomains].join(
-      "\\n",
-    );
-  } else if (typeof additionalDomains === "object") {
-    result.additionalDomains = additionalDomains;
-    result.domainList = [result.entryDomain, ...result.additionalDomains].join(
-      "\\n",
-    );
+  if (!sourceFilepath.endsWith('.handlebars')) {
+    console.log(chalk.gray(`copy ${sourceFilepath}`))
+    await fs.cp(sourceFilepath, destinationFilepath)
   } else {
-    result.domainList = [result.entryDomain];
+    console.log(chalk.blue(`render ${sourceFilepath}`))
+    const template = handlebars.compile(await fs.readFile(sourceFilepath, 'utf8'))
+    await fs.writeFile(destinationFilepath.replace(/\.handlebars$/, ''), template(config), 'utf8')
   }
-
-  result.smartDialerConfig = Buffer.from(smartDialerConfig).toString("base64");
-
-  return result;
 }
 
-if (decodeURI(import.meta.url).endsWith(process.argv[1])) {
-  const args = minimist(process.argv.slice(2));
+console.log(chalk.yellow('Copying mobileproxy files into the project...'))
+await fs.cp(SDK_MOBILEPROXY_OUTPUT_TARGET, WRAPPER_APP_OUTPUT_SDK_MOBILEPROXY_DIR, { recursive: true })
 
-  if (!args.platform) {
-    throw new Error(`Parameter \`--platform\` not provided.`);
-  }
+console.log(chalk.yellow('Installing external dependencies for the project...'))
+await promisify(exec)(`
+  cd ${WRAPPER_APP_OUTPUT_TARGET.replaceAll(' ', '\\ ')}
+  npm install --no-warnings
+  npx cap sync ${config.platform}
+`)
 
-  if (!args.entryUrl) {
-    throw new Error(`Parameter \`--entryUrl\` not provided.`);
-  }
+console.log(chalk.gray(`Zipping project to ${chalk.blue(WRAPPER_APP_OUTPUT_ZIP)}...`))
+await zip(WRAPPER_APP_OUTPUT_TARGET, WRAPPER_APP_OUTPUT_ZIP)
 
-  main({
-    ...args,
-    additionalDomains: args.additionalDomains?.split(/,\s*/) ?? [],
-  })
-    .catch(console.error);
-}
+console.log(chalk.bgGreen('Project ready!'))
