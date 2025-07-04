4c9e34250f02105bba28247b6c68a19625628fde
Grant Heaslip <grant@asl19.org>
2025-07-04 17:14:26 -0400

    Add TypeScript checking via JSDoc type annotations
    (Let me know if this is something you’re interested in at all -- I like
    having type safety for the project but I know TypeScript imposes some
    additional burden on contributors and maintainers.)
    
    I started this because while working on #472 I noticed getYAMLFileConfig
    had some additional edge cases where undefined could get returned, but
    figured I could fully implement type safety in wrapper_app_project
    while I was at it.
    
    Some notes:
    
    * Adds a basic tsconfig.json with "strict" enabled, but type checking in
    basic_navigator_example disabled for now. You can run type checking
    with a new "typescript:check" script.
    
    * Adds .vscode/settings.json to configure VS Code to do type checking
    and match the existing code style
    
    * I’m not super familiar with the config format so I may be making some
    incorrect assumptions here.

---
+++ x/examples/website-wrapper-app/wrapper_app_project/.scripts/types.mjs
@@ -0,0 +1,13 @@
+
+/**
+ * @typedef {{
+ *   additionalDomains: Array<string>;
+ *   appId: string;
+ *   appName: string;
+ *   domainList: string;
+ *   entryDomain: string;
+ *   output: string;
+ *   platform: string;
+ *   smartDialerConfig: string;
+ * }} Config
+ */
