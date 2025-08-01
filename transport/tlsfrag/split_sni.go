7eb0fd33a1e4b95d43c18cd87dedda8f25edd311
Sarah Laplante <laplante@google.com>
2025-08-01 10:17:04 +0200

    checkpoint

--- transport/tlsfrag/split_sni.go
+++ transport/tlsfrag/split_sni.go
@@ -30,6 +30,46 @@ import (
 // if the sniSplit is longer than the length of the SNI then no split happens
 // 15, example.com -> example.com
 
+// extract just the SNI extension from a client hello
+func getSNIExtension(clientHello []byte) ([]byte, error) {
+	// 6 bytes client hello start
+	// 32 bytes randomness
+	// 1 byte session id
+	// 2 bytes cipher suite length
+	// n bytes cipher suites
+	// 2 bytes compression info
+	// 2 bytes extension length
+
+	fmt.Printf("clientHello: %#x\n", clientHello)
+
+	helloTypeLength := 1
+	helloLengthLength := 3
+	tlsVersionLength := 2
+	clientRandomLength := 32
+
+	sessionDataIndex := helloTypeLength + helloLengthLength + tlsVersionLength + clientRandomLength
+
+	sessionDataLength := int(clientHello[sessionDataIndex])
+
+	fmt.Printf("session: %#v %v, %#x\n", sessionDataIndex, sessionDataLength, clientHello[sessionDataIndex-1:sessionDataIndex])
+
+	cipherSuiteLengthIndex := sessionDataIndex + 1 + sessionDataLength
+	cipherSuiteLength := int(binary.BigEndian.Uint16(clientHello[cipherSuiteLengthIndex : cipherSuiteLengthIndex+2]))
+
+	fmt.Printf("cipher: %#v %v, %#x\n", cipherSuiteLengthIndex, cipherSuiteLength, clientHello[cipherSuiteLengthIndex:cipherSuiteLengthIndex+2])
+
+	extensionLengthIndex := cipherSuiteLengthIndex + 2 + cipherSuiteLength + 2
+	extensionsLength := int(binary.BigEndian.Uint16(clientHello[extensionLengthIndex : extensionLengthIndex+2]))
+
+	fmt.Printf("extensions: %#v %v\n", extensionLengthIndex, extensionsLength)
+
+	extensionContent := clientHello[extensionLengthIndex+2 : extensionLengthIndex+2+extensionsLength]
+
+	fmt.Printf("extensionContent: %#v\n", extensionContent)
+
+	return extensionContent, nil
+}
+
 func MakeSplitSniFunc(sniSplit int) FragFunc {
 	// takes in an int, and returns a FragFunc which splits on the SNI
 
@@ -39,12 +79,23 @@ func MakeSplitSniFunc(sniSplit int) FragFunc {
 	// https://datatracker.ietf.org/doc/html/rfc6066#section-3
 	//sniHeader := []byte{0x00, 0x00, 0x00, 0x18, 0x00, 0x16, 0x00}
 
+	//            a   b   c   d   e   f   g   h   i
 	pattern := `\x00\x00\x00\x18\x00\x16\x00`
+	// a b = assigned value for server name extension
+	// c d = length of following server name extensino
+	// e f = length of first (and only) list entry
+	// g = entry type DNS hostname
+	// h i = length of hostname
+
 	re := regexp.MustCompile(pattern)
 
 	fragFunc := func(clientHello []byte) int {
 		fmt.Printf("clientHello: %#x\n", clientHello)
 		fmt.Printf("sniSplit: %d\n", sniSplit)
+
+		ext, _ := getSNIExtension(clientHello)
+
+		fmt.Printf("extensionContent: %#v\n", ext)
 
 		isMatch := re.Match(clientHello)
 		fmt.Printf("isMatch: %v\n", isMatch)
--- x/configurl/snifrag.go
+++ x/configurl/snifrag.go
@@ -26,16 +26,75 @@ import (
 )
 
 // Writing this here since tlsfrag.MakeSplitSniFunc is not accessible without a release
-// TODO move this function into transport/tlsfrag/split_sni.go
-func MakeSplitSniFunc(sniSplit int) tlsfrag.FragFunc {
-	// takes in an int, and returns a FragFunc which splits the on the sni
+// this allows direct testing through smart dialer
+// DO NOT SUBMIT
+// TODO delete this in favor of transport/tlsfrag/split_sni.go
 
+// -------------- COPY ZONE -----------------
+
+// Split SNI implemented FragFunc.
+
+// sniSplit can be positive or negative
+// positive splits forward in the sni, negative splits backward
+// 2, example.com -> ex ample.com
+// -5, example.com -> exam ple.com
+// but must always return a positive index value in the clientHello
+// if the sniSplit is longer than the length of the SNI then no split happens
+// 15, example.com -> example.com
+
+// extract just the SNI extension from a client hello
+func getSNIExtension(clientHello []byte) []byte {
+	// 6 bytes client hello start
+	// 32 bytes randomness
+	// 1 byte session id
+	// 2 bytes cipher suite length
+	// n bytes cipher suites
+	// 2 bytes compression info
+	// 2 bytes extension length
+
+	cipherSuiteLengthIndex := 6 + 32 + 1
+	cipherSuiteLength := int(binary.BigEndian.Uint16(clientHello[cipherSuiteLengthIndex : cipherSuiteLengthIndex+2]))
+
+	fmt.Printf("cipher: %#v %v\n", cipherSuiteLengthIndex, cipherSuiteLength)
+
+	extensionLengthIndex := cipherSuiteLengthIndex + 2 + cipherSuiteLength + 2
+	extensionsLength := int(binary.BigEndian.Uint16(clientHello[extensionLengthIndex : extensionLengthIndex+2]))
+
+	fmt.Printf("extensions: %#v %v\n", extensionLengthIndex, extensionsLength)
+
+	extensionContent := clientHello[extensionLengthIndex+2 : extensionLengthIndex+2+extensionsLength]
+
+	fmt.Printf("extensionContent: %#v\n", extensionContent)
+
+	return extensionContent
+}
+
+func MakeSplitSniFunc(sniSplit int) tlsfrag.FragFunc {
+	// takes in an int, and returns a FragFunc which splits on the SNI
+
+	// 00 00 00 18 00 16 00 [00 0n] ** 00
+	// represents the SNI extension + sni length + sni + next message
+	// ** (with no 00) represents the domain name
+	// https://datatracker.ietf.org/doc/html/rfc6066#section-3
+	//sniHeader := []byte{0x00, 0x00, 0x00, 0x18, 0x00, 0x16, 0x00}
+
+	//            a   b   c   d   e   f   g  h  i
 	pattern := `\x00\x00\x00\x18\x00\x16\x00`
+	// a b = assigned value for server name extension
+	// c d = length of following server name extensino
+	// e f = length of first (and only) list entry
+	// g = entry type DNS hostname
+	// h i = length of hostname
+
 	re := regexp.MustCompile(pattern)
 
 	fragFunc := func(clientHello []byte) int {
 		fmt.Printf("clientHello: %#x\n", clientHello)
 		fmt.Printf("sniSplit: %d\n", sniSplit)
+
+		ext := getSNIExtension(clientHello)
+
+		fmt.Printf("extensionContent: %#v\n", ext)
 
 		isMatch := re.Match(clientHello)
 		fmt.Printf("isMatch: %v\n", isMatch)
@@ -60,6 +119,8 @@ func MakeSplitSniFunc(sniSplit int) tlsfrag.FragFunc {
 
 	return fragFunc
 }
+
+// -------------- COPY ZONE -----------------
 
 func registerSNIFragStreamDialer(r TypeRegistry[transport.StreamDialer], typeID string, newSD BuildFunc[transport.StreamDialer]) {
 	r.RegisterType(typeID, func(ctx context.Context, config *Config) (transport.StreamDialer, error) {
