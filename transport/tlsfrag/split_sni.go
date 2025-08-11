564d80b398d912c7230486067be3aacd6562277e
Sarah Laplante <laplante@google.com>
2025-08-11 16:00:07 +0200

    some modifications to mod cases

--- transport/tlsfrag/split_sni.go
+++ transport/tlsfrag/split_sni.go
@@ -16,143 +16,17 @@ package tlsfrag
 
 import (
 	"bytes"
-	"encoding/binary"
-	"fmt"
-	"regexp"
 
 	"src.agwa.name/tlshacks"
 )
-
-// Split SNI implemented FragFunc.
 
 // sniSplit can be positive or negative
 // positive splits forward in the sni, negative splits backward
 // 2, example.com -> ex ample.com
 // -5, example.com -> exam ple.com
 // but must always return a positive index value in the clientHello
-// if the sniSplit is longer than the length of the SNI then no split happens
-// 15, example.com -> example.com
 
-// extract just the SNI extension from a client hello
-func getSNIExtension(clientHello []byte) ([]byte, error) {
-	// 6 bytes client hello start
-	// 32 bytes randomness
-	// 1 byte session id
-	// 2 bytes cipher suite length
-	// n bytes cipher suites
-	// 2 bytes compression info
-	// 2 bytes extension length
-
-	fmt.Printf("clientHello: %#x\n", clientHello)
-
-	helloTypeLength := 1
-	helloLengthLength := 3
-	tlsVersionLength := 2
-	clientRandomLength := 32
-
-	sessionDataIndex := helloTypeLength + helloLengthLength + tlsVersionLength + clientRandomLength
-
-	sessionDataLength := int(clientHello[sessionDataIndex])
-
-	fmt.Printf("session: %#v %v, %#x\n", sessionDataIndex, sessionDataLength, clientHello[sessionDataIndex-1:sessionDataIndex])
-
-	cipherSuiteLengthIndex := sessionDataIndex + 1 + sessionDataLength
-	cipherSuiteLength := int(binary.BigEndian.Uint16(clientHello[cipherSuiteLengthIndex : cipherSuiteLengthIndex+2]))
-
-	fmt.Printf("cipher: %#v %v, %#x\n", cipherSuiteLengthIndex, cipherSuiteLength, clientHello[cipherSuiteLengthIndex:cipherSuiteLengthIndex+2])
-
-	extensionLengthIndex := cipherSuiteLengthIndex + 2 + cipherSuiteLength + 2
-	extensionsLength := int(binary.BigEndian.Uint16(clientHello[extensionLengthIndex : extensionLengthIndex+2]))
-
-	fmt.Printf("extensions: %#v %v\n", extensionLengthIndex, extensionsLength)
-
-	allExtensionContent := clientHello[extensionLengthIndex+2 : extensionLengthIndex+2+extensionsLength]
-
-	fmt.Printf("extensionContent: %#v\n", allExtensionContent)
-
-	//firstExtIdentifier = allExtensionContent[0:2]
-	//if firstExtIdentifier != []byte{0x00, 0x00} {
-	//	return nil, Error("no SNI extension found in client hello")
-	//)
-
-	// sniExtLength
-
-	// sniExtension =
-
-	//return extensionContent, nil
-	return nil, nil
-}
-
-// The regex for a potential domain name.
-const domainRegexPattern = `(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}|xn--[a-z0-9-]+(?:\.[a-z0-9-]+)*`
-
-// Pre-compile the regex once at the package level.
-var domainRegex = regexp.MustCompile(domainRegexPattern)
-
-// findFirstDomainIndex takes a byte slice and returns the starting index of the
-// first string that matches the domain regex. If no match is found, it returns -1.
-func findFirstDomainIndex(data []byte) int {
-	// Use the pre-compiled regex directly.
-	matchIndexes := domainRegex.FindStringIndex(string(data))
-	if matchIndexes == nil {
-		return -1
-	}
-
-	return matchIndexes[0]
-}
-
-func OldMakeSplitSniFunc(sniSplit int) FragFunc {
-	// takes in an int, and returns a FragFunc which splits on the SNI
-
-	// 00 00 00 18 00 16 00 [00 0n] ** 00
-	// represents the SNI extension + sni length + sni + next message
-	// ** (with no 00) represents the domain name
-	// https://datatracker.ietf.org/doc/html/rfc6066#section-3
-	//sniHeader := []byte{0x00, 0x00, 0x00, 0x18, 0x00, 0x16, 0x00}
-
-	//            a   b   c   d   e   f   g   h   i
-	pattern := `\x00\x00\x00\x18\x00\x16\x00`
-	// a b = assigned value for server name extension
-	// c d = length of following server name extensino
-	// e f = length of first (and only) list entry
-	// g = entry type DNS hostname
-	// h i = length of hostname
-
-	re := regexp.MustCompile(pattern)
-
-	fragFunc := func(clientHello []byte) int {
-		fmt.Printf("clientHello: %#x\n", clientHello)
-		fmt.Printf("sniSplit: %d\n", sniSplit)
-
-		ext, _ := getSNIExtension(clientHello)
-
-		fmt.Printf("extensionContent: %#v\n", ext)
-
-		isMatch := re.Match(clientHello)
-		fmt.Printf("isMatch: %v\n", isMatch)
-
-		if isMatch {
-			sniExtensionIndex := re.FindIndex(clientHello)[0]
-			sniLengthBytes := clientHello[sniExtensionIndex+7 : sniExtensionIndex+9]
-			sniLength := int(binary.BigEndian.Uint16(sniLengthBytes))
-			sniStartIndex := sniExtensionIndex + 9
-
-			fmt.Printf("sniLength: %v\n", sniLength)
-			fmt.Printf("sniStartIndex: %v\n", sniStartIndex)
-
-			splitIndex := sniStartIndex + (sniSplit % sniLength)
-
-			fmt.Printf("splitIndex: %v\n", splitIndex)
-
-			return splitIndex
-		}
-		return 0
-	}
-
-	return fragFunc
-}
-
-func MakeSplitSniFunc(sniSplit int) FragFunc {
+func MakeSplitSniFunc(sniSplitOffset int) FragFunc {
 
 	fragFunc := func(clientHello []byte) int {
 		hello := tlshacks.UnmarshalClientHello(clientHello)
@@ -180,6 +54,10 @@ func MakeSplitSniFunc(sniSplit int) FragFunc {
 			// No SNI, don't split.
 			return 0
 		}
+		sniLength := len(serverName)
+
+		// Adjust sniSplits that are negative or longer than sniLength to the correct value
+		sniSplitOffset = sniSplitOffset % sniLength
 
 		sniIndex := bytes.Index(clientHello, []byte(serverName))
 		if sniIndex == -1 {
@@ -188,19 +66,7 @@ func MakeSplitSniFunc(sniSplit int) FragFunc {
 			return 0
 		}
 
-		sniLength := len(serverName)
-		splitOffset := sniSplit
-		if splitOffset < 0 {
-			// Handle negative split values, which count from the end of the SNI.
-			splitOffset = sniLength + splitOffset
-		}
-
-		if splitOffset <= 0 || splitOffset >= sniLength {
-			// Invalid split point (outside the SNI), don't split.
-			return 0
-		}
-
-		return sniIndex + splitOffset
+		return sniIndex + sniSplitOffset
 	}
 
 	return fragFunc
