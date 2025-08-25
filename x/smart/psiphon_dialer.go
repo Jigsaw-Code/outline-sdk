6153557ade02e72cd0b6c3f3d2e83f5ceb9de0b6
Sarah Laplante <laplante@google.com>
2025-08-25 15:37:16 +0200

    don't specify psiphon cachedir on android/ios

--- x/smart/psiphon_dialer.go
+++ x/smart/psiphon_dialer.go
@@ -20,14 +20,20 @@ func newPsiphonDialer(finder *StrategyFinder, ctx context.Context, psiphonJSON [
 
 	cacheBaseDir, err := os.UserCacheDir()
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get the user cache directory: %w", err)
-	}
+		// This call fails on android and ios, in which case we use the psiphon default
 
-	config.DataRootDirectory = path.Join(cacheBaseDir, "psiphon")
-	if err := os.MkdirAll(config.DataRootDirectory, 0700); err != nil {
-		return nil, fmt.Errorf("Failed to create storage directory: %w", err)
+		// https://github.com/Psiphon-Labs/psiphon-tunnel-core/blob/6560abad2812fa438b76ee6c0f74ee27a4aba82e/ClientLibrary/clientlib/clientlib.go#L40
+		// "Empty string means the default will be used (current working directory)."
+		finder.logCtx(ctx, "Failed to get user cache directory, using the psiphon default (current dir)\n")
 	}
-	finder.logCtx(ctx, "Using data store in %v\n", config.DataRootDirectory)
+	if err == nil {
+		config.DataRootDirectory = path.Join(cacheBaseDir, "psiphon")
+
+		if err := os.MkdirAll(config.DataRootDirectory, 0700); err != nil {
+			return nil, fmt.Errorf("Failed to create storage directory: %w", err)
+		}
+		finder.logCtx(ctx, "Using data store in %v\n", config.DataRootDirectory)
+	}
 
 	dialer := psiphon.GetSingletonDialer()
 	if err := dialer.Start(ctx, config); err != nil {
