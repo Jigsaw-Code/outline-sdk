b5aab51c8547677263362e5f434035ccf4db4925
Vinicius Fortuna <fortuna@users.noreply.github.com>
2025-08-27 11:11:38 -0400

    Tweak

--- run_on_podman.sh
+++ run_on_podman.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -eu
+
 function main() {
   declare -r bin="$1"
   # Remove the binary name from the args
