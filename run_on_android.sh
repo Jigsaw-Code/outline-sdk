1384a32fd4afd3f8b9ff98b1c70054cc012b5458
Vinicius Fortuna <fortuna@users.noreply.github.com>
2025-08-27 13:58:29 -0400

    Add Note

--- run_on_android.sh
+++ run_on_android.sh
@@ -14,6 +14,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This is currently failing on CI:
+#
+# + declare -r host_bin=/tmp/go-build323420225/b001/dns.test
+# ++ basename /tmp/go-build323420225/b001/dns.test
+# + declare -r android_bin=/data/local/tmp/test/dns.test
+# + adb push /tmp/go-build323420225/b001/dns.test /data/local/tmp/test/dns.test
+# /tmp/go-build323420225/b001/dns.test: 1 file pushed, 0 skipped. 119.2 MB/s (6852900 bytes in 0.055s)
+# + shift 1
+# + adb shell chmod +x /data/local/tmp/test/dns.test
+# + adb shell /data/local/tmp/test/dns.test -test.paniconexit0 -test.timeout=10m0s -test.v=true
+# /system/bin/sh: /data/local/tmp/test/dns.test: No such file or directory
+# FAIL	github.com/Jigsaw-Code/outline-sdk/dns	0.713s
+#
+# It's unclear why the binary file is not being found, despite the successful adb push.
+
 set -eu
 
 function main() {
