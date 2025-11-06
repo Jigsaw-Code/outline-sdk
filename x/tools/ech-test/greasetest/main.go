4a51b00c413ad1958e0e3e2f9da716e678e55806
Vinicius Fortuna <fortuna@users.noreply.github.com>
2025-11-06 15:48:35 -0500

    Add curl error code

--- x/tools/ech-test/greasetest/main.go
+++ x/tools/ech-test/greasetest/main.go
@@ -70,12 +70,101 @@ type TestResult struct {
 	Rank           int
 	ECHGrease      bool
 	Error          string
+	CurlExitCode   int
+	CurlErrorName  string
 	DNSLookup      time.Duration
 	TCPConnection  time.Duration
 	TLSHandshake   time.Duration
 	ServerTime     time.Duration
 	TotalTime      time.Duration
 	HTTPStatus     int
+}
+
+var curlExitCodeNames = map[int]string{
+	1:  "CURLE_UNSUPPORTED_PROTOCOL",
+	2:  "CURLE_FAILED_INIT",
+	3:  "CURLE_URL_MALFORMAT",
+	4:  "CURLE_NOT_BUILT_IN",
+	5:  "CURLE_COULDNT_RESOLVE_PROXY",
+	6:  "CURLE_COULDNT_RESOLVE_HOST",
+	7:  "CURLE_COULDNT_CONNECT",
+	8:  "CURLE_WEIRD_SERVER_REPLY",
+	9:  "CURLE_REMOTE_ACCESS_DENIED",
+	11: "CURLE_FTP_WEIRD_PASV_REPLY",
+	13: "CURLE_FTP_WEIRD_227_FORMAT",
+	14: "CURLE_FTP_CANT_GET_HOST",
+	15: "CURLE_FTP_CANT_RECONNECT",
+	17: "CURLE_FTP_COULDNT_SET_TYPE",
+	18: "CURLE_PARTIAL_FILE",
+	19: "CURLE_FTP_COULDNT_RETR_FILE",
+	21: "CURLE_QUOTE_ERROR",
+	22: "CURLE_HTTP_RETURNED_ERROR",
+	23: "CURLE_WRITE_ERROR",
+	25: "CURLE_UPLOAD_FAILED",
+	26: "CURLE_READ_ERROR",
+	27: "CURLE_OUT_OF_MEMORY",
+	28: "CURLE_OPERATION_TIMEDOUT",
+	30: "CURLE_FTP_PORT_FAILED",
+	31: "CURLE_FTP_COULDNT_USE_REST",
+	33: "CURLE_RANGE_ERROR",
+	34: "CURLE_HTTP_POST_ERROR",
+	35: "CURLE_SSL_CONNECT_ERROR",
+	36: "CURLE_BAD_DOWNLOAD_RESUME",
+	37: "CURLE_FILE_COULDNT_READ_FILE",
+	38: "CURLE_LDAP_CANNOT_BIND",
+	39: "CURLE_LDAP_SEARCH_FAILED",
+	41: "CURLE_FUNCTION_NOT_FOUND",
+	42: "CURLE_ABORTED_BY_CALLBACK",
+	43: "CURLE_BAD_FUNCTION_ARGUMENT",
+	45: "CURLE_INTERFACE_FAILED",
+	47: "CURLE_TOO_MANY_REDIRECTS",
+	48: "CURLE_UNKNOWN_OPTION",
+	49: "CURLE_TELNET_OPTION_SYNTAX",
+	51: "CURLE_PEER_FAILED_VERIFICATION",
+	52: "CURLE_GOT_NOTHING",
+	53: "CURLE_SSL_ENGINE_NOTFOUND",
+	54: "CURLE_SSL_ENGINE_SETFAILED",
+	55: "CURLE_SEND_ERROR",
+	56: "CURLE_RECV_ERROR",
+	58: "CURLE_SSL_CERTPROBLEM",
+	59: "CURLE_SSL_CIPHER",
+	60: "CURLE_SSL_CACERT",
+	61: "CURLE_BAD_CONTENT_ENCODING",
+	62: "CURLE_LDAP_INVALID_URL",
+	63: "CURLE_FILESIZE_EXCEEDED",
+	64: "CURLE_USE_SSL_FAILED",
+	65: "CURLE_SEND_FAIL_REWIND",
+	66: "CURLE_SSL_ENGINE_INITFAILED",
+	67: "CURLE_LOGIN_DENIED",
+	68: "CURLE_TFTP_NOTFOUND",
+	69: "CURLE_TFTP_PERM",
+	70: "CURLE_REMOTE_DISK_FULL",
+	71: "CURLE_TFTP_ILLEGAL",
+	72: "CURLE_TFTP_UNKNOWNID",
+	73: "CURLE_REMOTE_FILE_EXISTS",
+	74: "CURLE_TFTP_NOSUCHUSER",
+	75: "CURLE_CONV_FAILED",
+	76: "CURLE_CONV_REQD",
+	77: "CURLE_SSL_CACERT_BADFILE",
+	78: "CURLE_REMOTE_FILE_NOT_FOUND",
+	79: "CURLE_SSH",
+	80: "CURLE_SSL_SHUTDOWN_FAILED",
+	81: "CURLE_AGAIN",
+	82: "CURLE_SSL_CRL_BADFILE",
+	83: "CURLE_SSL_ISSUER_ERROR",
+	84: "CURLE_FTP_PRET_FAILED",
+	85: "CURLE_RTSP_CSEQ_ERROR",
+	86: "CURLE_RTSP_SESSION_ERROR",
+	87: "CURLE_FTP_BAD_FILE_LIST",
+	88: "CURLE_CHUNK_FAILED",
+	89: "CURLE_NO_CONNECTION_AVAILABLE",
+	90: "CURLE_SSL_PINNEDPUBKEYNOTMATCH",
+	91: "CURLE_SSL_INVALIDCERTSTATUS",
+	92: "CURLE_HTTP2_STREAM",
+	93: "CURLE_RECURSIVE_API_CALL",
+	94: "CURLE_AUTH_ERROR",
+	95: "CURLE_HTTP3",
+	96: "CURLE_QUIC_CONNECT_ERROR",
 }
 
 func runTest(curlPath string, domain Domain, echGrease bool) TestResult {
@@ -109,6 +198,10 @@ func runTest(curlPath string, domain Domain, echGrease bool) TestResult {
 	err := cmd.Run()
 	if err != nil {
 		result.Error = fmt.Sprintf("failed to run curl: %v, stderr: %s", err, stderr.String())
+		if exitError, ok := err.(*exec.ExitError); ok {
+			result.CurlExitCode = exitError.ExitCode()
+			result.CurlErrorName = curlExitCodeNames[result.CurlExitCode]
+		}
 		return result
 	}
 
@@ -259,7 +352,7 @@ func main() {
 	csvWriter := csv.NewWriter(outputFile)
 	defer csvWriter.Flush()
 
-	header := []string{"domain", "rank", "ech_grease", "error", "dns_lookup_ms", "tcp_connection_ms", "tls_handshake_ms", "server_time_ms", "total_time_ms", "http_status"}
+	header := []string{"domain", "rank", "ech_grease", "error", "curl_exit_code", "curl_error_name", "dns_lookup_ms", "tcp_connection_ms", "tls_handshake_ms", "server_time_ms", "total_time_ms", "http_status"}
 	if err := csvWriter.Write(header); err != nil {
 		slog.Error("Failed to write CSV header", "error", err)
 		os.Exit(1)
@@ -277,6 +370,8 @@ func main() {
 				strconv.Itoa(result.Rank),
 				strconv.FormatBool(result.ECHGrease),
 				result.Error,
+				strconv.Itoa(result.CurlExitCode),
+				result.CurlErrorName,
 				strconv.FormatInt(result.DNSLookup.Milliseconds(), 10),
 				strconv.FormatInt(result.TCPConnection.Milliseconds(), 10),
 				strconv.FormatInt(result.TLSHandshake.Milliseconds(), 10),
